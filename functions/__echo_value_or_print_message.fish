253057160b31434348cc14aa039c8e8801dfd03b
Patrick Fong <patrickf3139@gmail.com>
2020-04-30 15:50:16 -0700

    use a helper function to print the value of variables

---
+++ functions/__echo_value_or_print_message.fish
@@ -0,0 +1,7 @@
+function __echo_value_or_print_message --description "Echo the value of the variable passed in or a message that it is not available." --argument-names variable_name
+    if set --query $variable_name
+        echo $$variable_name
+    else
+        echo "----- $variable_name was not exported to this process so its value cannot be printed -----"
+    end
+end
--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -1,12 +1,12 @@
 function __fzf_search_shell_variables --description "Search shell variables using fzf. Insert the selected variable into the commandline at the cursor."
     # list all the shell variables names, preview the value of the selected variable in fzf's preview window
     # if the variable has an empty value, notify the user that its value is empty or cannot be accessed
-    # hack borrowed from https://unix.stackexchange.com/a/88682/246672
+    function test
+        echo test
+    end
     set variable_name (
         set --names |
-        fzf --height 40% --layout=reverse --preview 'set shell_var_name {}; and \
-        set shell_var_value $$shell_var_name ">>> \$$shell_var_name is empty or it was not exported to fzf process so its value cannot be printed <<<"; and \
-        echo $shell_var_value[1]'
+        fzf --height 40% --layout=reverse --preview '__echo_value_or_print_message {}'
     )
 
     if test $status -eq 0
