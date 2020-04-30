cdf5fbf16f85fe7aa8c376f7f1eb7878b2156c14
Patrick Fong <patrickf3139@gmail.com>
2020-04-30 14:10:46 -0700

    reverse layout of fzf

--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -2,7 +2,7 @@ function __fzf_search_shell_variables --description "Search shell variables usin
     # list all the shell variables names, preview the value of the selected variable in fzf's preview window
     set variable_name (
         set --names |
-        fzf --height 40% --preview 'set shell_variable_name {}; echo $$shell_variable_name'
+        fzf --height 40% --layout=reverse --preview 'set shell_variable_name {}; echo $$shell_variable_name'
     )
 
     if test $status -eq 0
