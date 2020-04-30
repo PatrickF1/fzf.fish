88505316972aaf3539f098d40354a84e0b69a3a3
Patrick Fong <patrickf3139@gmail.com>
2020-04-30 13:24:40 -0700

    preview value as a second column

--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -1,8 +1,11 @@
 function __fzf_search_shell_variables --description "Search shell variables using fzf. Insert the selected variable into the commandline at the cursor."
-    set variable_and_value (set | fzf --height 40%)
+    set variable_name (
+        set --names |
+        fzf --height 40% --preview 'set shell_variable_name {}; echo $$shell_variable_name'
+    )
 
     if test $status -eq 0
-        commandline --replace $variable_and_value[1] # insert only the variable name and exclude the value
+        commandline --replace $variable[1]
     end
 
     commandline --function repaint
