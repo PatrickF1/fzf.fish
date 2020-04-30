7eb3739593311ac69e602411bbb3e2284c6084b9
Patrick Fong <patrickf3139@gmail.com>
2020-04-30 15:53:31 -0700

    clean up

--- functions/__echo_value_or_print_message.fish
+++ functions/__echo_value_or_print_message.fish
@@ -1,3 +1,4 @@
+# helper function for search shell variable functionality
 function __echo_value_or_print_message --description "Echo the value of the variable passed in or a message that it is not available." --argument-names variable_name
     if set --query $variable_name
         echo $$variable_name
--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -1,9 +1,6 @@
-function __fzf_search_shell_variables --description "Search shell variables using fzf. Insert the selected variable into the commandline at the cursor."
+function __fzf_search_shell_variables --description "Search and inspect shell variables using fzf. Insert the selected variable into the commandline at the cursor."
     # list all the shell variables names, preview the value of the selected variable in fzf's preview window
     # if the variable has an empty value, notify the user that its value is empty or cannot be accessed
-    function test
-        echo test
-    end
     set variable_name (
         set --names |
         fzf --height 40% --layout=reverse --preview '__echo_value_or_print_message {}'
