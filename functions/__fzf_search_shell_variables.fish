27a4100a3f1a73d0dfdf4af16497442aee6f7ef9
Patrick Fong <patrickf3139@gmail.com>
2020-04-30 13:29:26 -0700

    add commentary

--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -1,4 +1,5 @@
 function __fzf_search_shell_variables --description "Search shell variables using fzf. Insert the selected variable into the commandline at the cursor."
+    # list all the shell variables names, preview the value of the selected variable in fzf's preview window
     set variable_name (
         set --names |
         fzf --height 40% --preview 'set shell_variable_name {}; echo $$shell_variable_name'
