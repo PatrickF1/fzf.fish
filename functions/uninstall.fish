e6c85875e759f216e7c966939cc05f0a0660c33d
Patrick Fong <patrickf3139@gmail.com>
2020-07-20 20:51:47 -0700

    add note in uninstall.fish

--- functions/uninstall.fish
+++ functions/uninstall.fish
@@ -1,4 +1,5 @@
 #!/usr/bin/env fish
+# --erase does not error if keybinding is not set, so don't have to account for fzf_fish_custom_keybindings
 bind --erase \cf
 bind --erase \cl
 bind --erase \cr
