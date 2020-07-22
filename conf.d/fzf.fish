9854a2a2256ffe625324270d25d47df822652552
Patrick Fong <patrickf3139@gmail.com>
2020-07-21 21:37:48 -0700

    reduce documentation

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -1,4 +1,4 @@
-# Set up some default keybindings if user has not chosen to customize them
+# Set up the default, mnemonic keybindings unless the user has chosen to customize them
 if not set --query fzf_fish_custom_keybindings
     # \cf is ctrl+f, etc.
     bind \cf '__fzf_search_current_dir'
@@ -7,10 +7,8 @@ if not set --query fzf_fish_custom_keybindings
     bind \cv '__fzf_search_shell_variables'
 end
 
-# fzf automatically looks for the FZF_DEFAULT_OPTS environment variable and if it is set, its value
-# is automatically passed into every invocation of fzf.
+# If FZF_DEFAULT_OPTS is not set, then set some sane defaults. This also affects fzf outside of this plugin.
 # See https://github.com/junegunn/fzf#environment-variables
-# If FZF_DEFAULT_OPTS is not set, then set some sane defaults. This also affects fzf outside this plugin.
 if not set --query FZF_DEFAULT_OPTS
-    set -g FZF_DEFAULT_OPTS '--cycle --reverse --border --height 75%'
+    set --export FZF_DEFAULT_OPTS '--cycle --reverse --border --height 75%'
 end
