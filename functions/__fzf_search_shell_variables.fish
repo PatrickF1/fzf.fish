171b586b5cec967ee0a43b338ae51e6c3aa57388
Patrick Fong <patrickf3139@gmail.com>
2020-07-22 15:50:37 -0700

    Wrap long lines in variable value preview window

--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -4,13 +4,13 @@ function __fzf_search_shell_variables --description "Search and inspect shell va
     # Using --local so that it does not clobber SHELL outside of this function.
     set --local --export SHELL (which fish)
 
-    # Pipe the names of all shell variables to fzf.
-    # Attempt to display the value of the selected variable in fzf's preview window.
+    # Pipe the names of all shell variables to fzf and attempt to display the value
+    # of the selected variable in fzf's preview window.
     # Non-exported variables will not be accessible to the fzf process, in which case
     # __echo_value_or_print_message will print an informative message in lieu of the value.
     set variable_name (
         set --names |
-        fzf --preview '__echo_value_or_print_message {}'
+        fzf --preview '__echo_value_or_print_message {}' --preview-window wrap
     )
 
     if test $status -eq 0
