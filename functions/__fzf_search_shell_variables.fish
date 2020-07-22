62eec2450d33899db9a37aaed5552cf41efe8720
Patrick <patrickf3139@gmail.com>
2020-07-22 13:21:57 -0700

    Make sure fzf uses fish to preview environment variables (#11)
    Resolves a bug where previewing the value of shell variables doesn't work when SHELL != fill.

--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -1,4 +1,9 @@
 function __fzf_search_shell_variables --description "Search and inspect shell variables using fzf. Insert the selected variable into the commandline at the cursor."
+    # Make sure that fzf uses fish to execute __echo_value_or_print_message, which
+    # is an autoloaded fish function so doesn't exist in other shells.
+    # Using --local so that it does not clobber SHELL outside of this function.
+    set --local --export SHELL (which fish)
+
     # Pipe the names of all shell variables to fzf.
     # Attempt to display the value of the selected variable in fzf's preview window.
     # Non-exported variables will not be accessible to the fzf process, in which case
