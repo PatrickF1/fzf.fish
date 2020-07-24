8a5c255eb90361c6538a020649912a680bf7738f
Patrick Fong <patrickf3139@gmail.com>
2020-07-24 16:43:03 -0700

    save progress

--- functions/__fzf_search_history.fish
+++ functions/__fzf_search_history.fish
@@ -1,12 +1,14 @@
 # originally implemented and transposed from https://github.com/patrickf3139/dotfiles/pull/11
 function __fzf_search_history --description "Search command history using fzf. Replace the commandline with the selected command."
     history merge
-    set command_selected (
-        history --null |
+    set command_with_ts (
+        # Reference https://devhints.io/strftime to understand strftime format symbols
+        history --null --show-time="%m/%e %I:%M %p | " |
         fzf --read0 --tiebreak=index --query=(commandline)
     )
 
     if test $status -eq 0
+        set command_selected (string split --max 1 " | " $command_with_ts)[]
         commandline --replace $command_selected
     end
 
