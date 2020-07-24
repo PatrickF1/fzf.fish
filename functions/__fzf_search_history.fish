bb4ad15c5fb1cc0e9e83e8ae7f613ad7ec06bc98
Patrick Fong <patrickf3139@gmail.com>
2020-07-24 16:53:01 -0700

    show seconds

--- functions/__fzf_search_history.fish
+++ functions/__fzf_search_history.fish
@@ -1,9 +1,10 @@
 # originally implemented and transposed from https://github.com/patrickf3139/dotfiles/pull/11
 function __fzf_search_history --description "Search command history using fzf. Replace the commandline with the selected command."
+    # history merge incorporates history changes from other fish sessions
     history merge
     set command_with_ts (
         # Reference https://devhints.io/strftime to understand strftime format symbols
-        history --null --show-time="%m/%e %I:%M %p | " |
+        history --null --show-time="%m/%e %H:%M:%S | " |
         fzf --read0 --tiebreak=index --query=(commandline)
     )
 
