3ef6992da7801984155fa1a96fd53325d480faf9
Patrick <patrickf3139@gmail.com>
2020-07-24 16:55:29 -0700

    Show command timestamps when searching command history (#12)
    Making full use of the timestamp option for https://fishshell.com/docs/current/cmds/history.html.
    New command history line example: `07/20 20:18:12 | help bind`.

--- functions/__fzf_search_history.fish
+++ functions/__fzf_search_history.fish
@@ -1,12 +1,15 @@
 # originally implemented and transposed from https://github.com/patrickf3139/dotfiles/pull/11
 function __fzf_search_history --description "Search command history using fzf. Replace the commandline with the selected command."
+    # history merge incorporates history changes from other fish sessions
     history merge
-    set command_selected (
-        history --null |
+    set command_with_ts (
+        # Reference https://devhints.io/strftime to understand strftime format symbols
+        history --null --show-time="%m/%e %H:%M:%S | " |
         fzf --read0 --tiebreak=index --query=(commandline)
     )
 
     if test $status -eq 0
+        set command_selected (string split --max 1 " | " $command_with_ts)[2]
         commandline --replace $command_selected
     end
 
