7d16ece30b6963e41c03fb5d659854a46931c6f5
Patrick Fong <patrickf3139@gmail.com>
2020-08-09 11:08:42 -0700

    Only erase keybindings if using default keybindings
    Previously, I figured that since bind --erase is idempotent, we can always try erasing keybindings. However, that led to "fzf_fish_integration key kindings removed" being printed even if the user had set their own keybindings and that can be confusing. Therefore, I decided to only erase keybindings and print that message if the user had not elected to use custom keybindings.

--- functions/uninstall.fish
+++ functions/uninstall.fish
@@ -1,9 +1,13 @@
 #!/usr/bin/env fish
-# --erase does not error if keybinding is not set, so don't have to account for fzf_fish_custom_keybindings
-bind --erase \cf
-bind --erase \cl
-bind --erase \cr
-bind --erase \cv
-echo "fzf_fish_integration key kindings removed"
+if not set --query fzf_fish_custom_keybindings
+    bind --erase \cf
+    bind --erase \cl
+    bind --erase \cr
+    bind --erase \cv
+
+    set_color --italics cyan
+    echo "fzf_fish_integration key kindings removed"
+    set_color normal
+end
 
 # Not going to erase FZF_DEFAULT_OPTS because too hard to tell if it set by the user or by this plugin
