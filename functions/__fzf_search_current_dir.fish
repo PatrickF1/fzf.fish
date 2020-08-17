17b5dc80662ee3b920320329c29d053f0b8cd6de
Patrick Fong <patrickf3139@gmail.com>
2020-08-17 16:47:38 -0700

    --preview-window=wrap

--- functions/__fzf_search_current_dir.fish
+++ functions/__fzf_search_current_dir.fish
@@ -5,7 +5,7 @@ function __fzf_search_current_dir --description "Search the current directory us
     set --local --export SHELL (which fish)
     set file_path_selected (
         fd --hidden --follow --color=always --exclude=.git 2> /dev/null |
-        fzf --ansi --preview='__fzf_preview_file {}' --preview-window=wrap
+        fzf --ansi --preview='__fzf_preview_file {}'
     )
 
     if test $status -eq 0
