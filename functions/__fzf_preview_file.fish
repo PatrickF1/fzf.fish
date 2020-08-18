387f2c1cbfefdf11c62fa1d9f87ad87c17cf3fa8
Patrick Fong <patrickf3139@gmail.com>
2020-08-18 08:41:01 -0700

    fix?

--- functions/__fzf_preview_file.fish
+++ functions/__fzf_preview_file.fish
@@ -1,19 +1,23 @@
 # helper function for __fzf_search_current_dir
 function __fzf_preview_file --description "Prints a preview for the given file based on its file type." --argument-names file_path
-    set file_type (file --brief -i "$file_path")
-    switch $file_type
-        case "directory"
+    # -o gives long format without group id
+    # -d does not display the contents of named directories, but information about the directories themselves
+    # We want long format because it outputs the file type symbol as the very first character of each row
+    # See https://linuxconfig.org/identifying-file-types-in-linux
+    set file_symbol (ls -o -d "$file_path" | string sub --length 1)
+    switch $file_symbol
+        case b c
+            __fzf_report_file_type "$file_path" "device file"
+        case d
             # Setting CLICOLOR_FORCE forces colors to be enabled even to a non-terminal output
             CLICOLOR_FORCE=true ls -a "$file_path"
-        case "socket"
+        case s
             __fzf_report_file_type "$file_path" "socket"
-        case "broken symbolic link*" # example: "broken symbolic link to /path/to/file""
-            __fzf_report_file_type "$file_path" "broken symbolic link"
-        case "* special *" # examples: "character special (23/5)" and "block special (1/7)"
-            __fzf_report_file_type "$file_path" "device file"
-        case "regular file"
+        case p
+            __fzf_report_file_type "$file_path" "???"
+        case - l # TODO: test l for directories
             bat --style=numbers --color=always "$file_path"
         case "*"
-            echo "Unsure how to preview '$file_path', which has file type '$file_type'. Please open an issue at https://github.com/patrickf3139/fzf.fish."
+            echo "Unsure how to preview '$file_path', which has file symbol $file_symbol. Please open an issue at https://github.com/patrickf3139/fzf.fish."
     end
 end
