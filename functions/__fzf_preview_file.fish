129708fd78363f8af895fc5f9eecfcf7d641d35b
Patrick Fong <patrickf3139@gmail.com>
2020-08-18 13:10:44 -0700

    fix colors

--- functions/__fzf_preview_file.fish
+++ functions/__fzf_preview_file.fish
@@ -16,7 +16,11 @@ function __fzf_preview_file --description "Prints a preview for the given file b
         case l # symlink
             # notify user and recurse on the target of the symlink, which can be any of these file types
             set target_path (realpath $file_path)
-            set_color yellow; and echo "'$file_path' is a symlink to '$target_path'."
+
+            set_color yellow
+            echo "'$file_path' is a symlink to '$target_path'."
+            set_color normal
+
             __fzf_preview_file "$target_path"
         case c
             __fzf_report_file_type "$file_path" "character device file"
@@ -27,9 +31,11 @@ function __fzf_preview_file --description "Prints a preview for the given file b
         case p
             __fzf_report_file_type "$file_path" "named pipe"
         case "" # occurs when ls fails, e.g. with bad file descriptors
-            set_color red; and echo 'ls command failed.' >&2
+            set_color red
+            echo 'ls command failed.' >&2
+            set_color normal
             exit 1
         case "*"
-            echo "Unexpected file symbol $file_symbol. Please open an issue at https://github.com/patrickf3139/fzf.fish."
+            echo "Unexpected file symbol $file_symbol. Please open an issue at https://github.com/patrickf3139/fzf.fish." >&2
     end
 end
