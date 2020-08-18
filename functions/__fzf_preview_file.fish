51ffdaaa93fb02acd276e18fdc214ac5db59c02d
Patrick Fong <patrickf3139@gmail.com>
2020-08-18 11:08:41 -0700

    document more

--- functions/__fzf_preview_file.fish
+++ functions/__fzf_preview_file.fish
@@ -6,15 +6,15 @@ function __fzf_preview_file --description "Prints a preview for the given file b
     # See https://linuxconfig.org/identifying-file-types-in-linux
     set file_symbol (ls -o -d "$file_path" | string sub --length 1)
     switch $file_symbol
-        case b c
+        case b c # block and character device file
             __fzf_report_file_type "$file_path" "device file"
         case d
             # Setting CLICOLOR_FORCE forces colors to be enabled even to a non-terminal output
             CLICOLOR_FORCE=true ls -a "$file_path"
         case s
             __fzf_report_file_type "$file_path" "socket"
-        case p
-            __fzf_report_file_type "$file_path" "???"
+        case p # named pipe
+            __fzf_report_file_type "$file_path" TODO
         case - l # TODO: test l for directories
             bat --style=numbers --color=always "$file_path"
         case "*"
