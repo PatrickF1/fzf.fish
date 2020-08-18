7ebe4ed6734ad1441636743364783ae6c8a7859d
Patrick <patrickf3139@gmail.com>
2020-08-18 13:16:38 -0700

    Fix file preview not working non non-Mac OSes (#21)
    # Problem
    File preview wasn't working on Linux and presumably Windows because the file command that ships with Mac, used to determine the file type, is outdated and has different options.
    
    # Solution
    Use ls to get the file type instead. Big thanks to @chshbh for the idea of using ls! He opened up a great PR that this is based on.

--- functions/__fzf_preview_file.fish
+++ functions/__fzf_preview_file.fish
@@ -1,19 +1,41 @@
 # helper function for __fzf_search_current_dir
 function __fzf_preview_file --description "Prints a preview for the given file based on its file type." --argument-names file_path
-    set file_type (file --brief -i "$file_path")
-    switch $file_type
-        case "directory"
+    # -d displays information about the directories themselves instead of the contents of the directories
+    # -o displays long format without group id
+    # We want long format because it outputs the file type symbol as the very first character of each row
+    set file_symbol (ls -o -d "$file_path" | string sub --length 1)
+
+    # For more on file symbols outputted by ls long format, see https://linuxconfig.org/identifying-file-types-in-linux
+    # For more on file types, see https://en.wikipedia.org/wiki/Unix_file_types
+    switch $file_symbol
+        case - # regular file
+            bat --style=numbers --color=always "$file_path"
+        case d # directory
             # Setting CLICOLOR_FORCE forces colors to be enabled even to a non-terminal output
             CLICOLOR_FORCE=true ls -a "$file_path"
-        case "socket"
+        case l # symlink
+            # notify user and recurse on the target of the symlink, which can be any of these file types
+            set target_path (realpath $file_path)
+
+            set_color yellow
+            echo "'$file_path' is a symlink to '$target_path'."
+            set_color normal
+
+            __fzf_preview_file "$target_path"
+        case c
+            __fzf_report_file_type "$file_path" "character device file"
+        case b
+            __fzf_report_file_type "$file_path" "block device file"
+        case s
             __fzf_report_file_type "$file_path" "socket"
-        case "broken symbolic link*" # example: "broken symbolic link to /path/to/file""
-            __fzf_report_file_type "$file_path" "broken symbolic link"
-        case "* special *" # examples: "character special (23/5)" and "block special (1/7)"
-            __fzf_report_file_type "$file_path" "device file"
-        case "regular file"
-            bat --style=numbers --color=always "$file_path"
+        case p
+            __fzf_report_file_type "$file_path" "named pipe"
+        case "" # occurs when ls fails, e.g. with bad file descriptors
+            set_color red
+            echo 'ls command failed.' >&2
+            set_color normal
+            exit 1
         case "*"
-            echo "Unsure how to preview '$file_path', which has file type '$file_type'. Please open an issue at https://github.com/patrickf3139/fzf.fish."
+            echo "Unexpected file symbol $file_symbol. Please open an issue at https://github.com/patrickf3139/fzf.fish." >&2
     end
 end
--- functions/__fzf_report_file_type.fish
+++ functions/__fzf_report_file_type.fish
@@ -1,6 +1,6 @@
 # helper function for __fzf_preview_file
 function __fzf_report_file_type --description "Explain the file type for a file." --argument-names file_path file_type
     set_color red
-    echo "Cannot preview: '$file_path' is a $file_type."
+    echo "Cannot preview '$file_path': it is a $file_type."
     set_color normal
 end
