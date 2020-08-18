3450ed3ba4e345b830655b1c5356b4a3787ac6cc
Patrick Fong <patrickf3139@gmail.com>
2020-08-18 11:58:26 -0700

    reorder

--- functions/__fzf_preview_file.fish
+++ functions/__fzf_preview_file.fish
@@ -8,23 +8,23 @@ function __fzf_preview_file --description "Prints a preview for the given file b
     # For more on file symbols outputted by ls long format, see https://linuxconfig.org/identifying-file-types-in-linux
     # For more on file types, see https://en.wikipedia.org/wiki/Unix_file_types
     switch $file_symbol
-        case b c # block and character device file
-            __fzf_report_file_type "$file_path" "device file"
-        case d
+        case - # regular file
+            bat --style=numbers --color=always "$file_path"
+        case d # directory
             # Setting CLICOLOR_FORCE forces colors to be enabled even to a non-terminal output
             CLICOLOR_FORCE=true ls -a "$file_path"
+        case b c # block and character device file
+            __fzf_report_file_type "$file_path" "device file"
         case s
             __fzf_report_file_type "$file_path" "socket"
         case p
             __fzf_report_file_type "$file_path" "named pipe"
-        case -
-            bat --style=numbers --color=always "$file_path"
-        case l
-            # handle symlinks by notifying user and recursing on the target of the symlink as it could be any of these file types
+        case l # symlink
+            # notify user and recurse on the target of the symlink, which can be any of these file types
             set target_path (realpath $file_path)
             set_color yellow; and echo "'$file_path' is a symlink to '$target_path'"
             __fzf_preview_file "$target_path"
         case "*"
-            echo "Unsure how to preview '$file_path', which has file symbol $file_symbol. Please open an issue at https://github.com/patrickf3139/fzf.fish."
+            echo "Unexpected file symbol $file_symbol. Please open an issue at https://github.com/patrickf3139/fzf.fish."
     end
 end
