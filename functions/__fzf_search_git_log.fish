7adc136131f386c870f79bda4b082ae07a80dbe5
Patrick Fong <patrickf3139@gmail.com>
2020-09-07 18:21:41 -0700

    show colors in git diff

--- functions/__fzf_search_git_log.fish
+++ functions/__fzf_search_git_log.fish
@@ -7,7 +7,7 @@ function __fzf_search_git_log --description "Search the git log of the current g
             # see documentation for git format placeholders at https://git-scm.com/docs/git-log#Documentation/git-log.txt-emnem
             # %h gives you the abbreviated commit hash, which is useful for saving screen space, but we will have to expand it later below
             git log --color=always --format=format:'%C(bold blue)%h%C(reset) - %C(cyan)%as%C(reset) %C(yellow)%d%C(reset) %C(normal)%s%C(reset)  %C(dim normal)[%an]%C(reset)' | \
-            fzf --ansi --tiebreak=index --preview='git show (string split --max 1 " " {})[1]'
+            fzf --ansi --tiebreak=index --preview='git show --color=always (string split --max 1 " " {})[1]'
         )
         if test $status -eq 0
             set abbreviated_commit_hash (string split --max 1 " " $selected_log_line)[1]
