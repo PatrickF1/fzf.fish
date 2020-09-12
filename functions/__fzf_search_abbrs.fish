89cc9cd4061432d17be89d5d11e9b88dedeaffcc
matthiasdebernardini <m.f.debernardini@student.vu.nl>
2020-09-12 19:39:24 +0000

    Added extra file, and added a keybinding

--- functions/__fzf_search_abbrs.fish
+++ functions/__fzf_search_abbrs.fish
@@ -1,21 +1,20 @@
-function __fzf_search_shell_variables --description "Search and inspect shell variables using fzf. Insert the selected variable into the commandline at the cursor."
+function __fzf_search_abbreviations --description "Search and inspect abbreviations using fzf.
+Insert the commands youre interested in into the commandline at the cursor."
     # Make sure that fzf uses fish to execute __echo_value_or_print_message, which
     # is an autoloaded fish function so doesn't exist in other shells.
     # Using --local so that it does not clobber SHELL outside of this function.
     set --local --export SHELL (which fish)
 
-    # Pipe the names of all shell variables to fzf and attempt to display the value
-    # of the selected variable in fzf's preview window.
-    # Non-exported variables will not be accessible to the fzf process, in which case
-    # __echo_value_or_print_message will print an informative message in lieu of the value.
-    set variable_name (
-        cat .config/fish/conf.d/abbreviations.fish |
-        grep abbr |
-        fzf --preview '__fzf_display_value_or_error {}'
+    # Pipe the names of all abreviations in your .config to fzf and attempt to display the value
+    # of the selected abbr in fzf's preview window.
+    set abbreviations (
+           # this pipes the output of any file whose name matches with abbr inside of a dot config
+           # folder for fish into bat  
+           bat (fd -F abbr ~/.config/fish/) |
+           fzf --preview '__fzf_display_value_or_error {}'
     )
-
     if test $status -eq 0
-        commandline --insert $variable_name
+        commandline --insert $abbreviations
     end
 
     commandline --function repaint
