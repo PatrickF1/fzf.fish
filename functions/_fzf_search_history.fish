d0584c9e60b315bf4ab6047da5cf8b5eb15e1c72
Ilan Cosman <ilancosman@gmail.com>
2020-09-21 22:27:43 -0700

    Minor edits

--- functions/_fzf_search_history.fish
+++ functions/_fzf_search_history.fish
@@ -2,13 +2,9 @@
 function _fzf_search_history --description "Search command history using fzf. Replace the commandline with the selected command."
     # history merge incorporates history changes from other fish sessions
     history merge
-    set -l commandWithTs (
-        # Reference https://devhints.io/strftime to understand strftime format symbols
-        history --null --show-time="%m/%e %H:%M:%S | " |
-        fzf --read0 --tiebreak=index --query=(commandline)
-    )
 
-    if test $status -eq 0
+    # Reference https://devhints.io/strftime to understand strftime format symbols
+    if set -l commandWithTs (history --null --show-time="%m/%e %H:%M:%S | " | fzf --read0 --tiebreak=index --query=(commandline))
         set -l commandSelected (string split --max 1 " | " $commandWithTs)[2]
         commandline --replace $commandSelected
     end
