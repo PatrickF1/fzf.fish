b1f716af500f89d3fc74d5a60ac82ad370a2ab91
Ilan Cosman <ilancosman@gmail.com>
2020-09-21 21:42:43 -0700

    Use no-scope-shadowing in report_file

--- functions/__fzf_preview_file.fish
+++ functions/__fzf_preview_file.fish
@@ -4,6 +4,7 @@ function __fzf_preview_file --description "Prints a preview for the given file b
         bat --style=numbers --color=always "$file_path"
         echo hello
     else if test -d "$file_path" # directory
+        # Setting CLICOLOR_FORCE forces colors to be enabled even to a non-terminal output
         CLICOLOR_FORCE=true ls -a "$file_path"
     else if test -L "$file_path" # symlink
         # notify user and recurse on the target of the symlink, which can be any of these file types
@@ -15,13 +16,13 @@ function __fzf_preview_file --description "Prints a preview for the given file b
 
         __fzf_preview_file "$target_path"
     else if test -c "$file_path"
-        __fzf_report_file_type "$file_path" "character device file"
+        __fzf_report_file_type "character device file"
     else if test -b "$file_path"
-        __fzf_report_file_type "$file_path" "block device file"
+        __fzf_report_file_type "block device file"
     else if test -S "$file_path"
-        __fzf_report_file_type "$file_path" "socket"
+        __fzf_report_file_type "socket"
     else if test -p "$file_path"
-        __fzf_report_file_type "$file_path" "named pipe"
+        __fzf_report_file_type "named pipe"
     else
         echo "Unexpected file symbol $file_type_char. Please open an issue at https://github.com/patrickf3139/fzf.fish." >&2
     end
