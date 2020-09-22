d44f95f42be75e7f39544bb7e07f18db3006fd78
Ilan Cosman <ilancosman@gmail.com>
2020-09-22 13:01:48 -0700

    Make git_status commandline repaint consistent with git_log

--- functions/_fzf_search_git_status.fish
+++ functions/_fzf_search_git_status.fish
@@ -2,22 +2,20 @@ function _fzf_search_git_status -d "Search the git status of the current git rep
     # Pass configuration color.status=always to force status to use colors even though output is sent to a pipe
     if not set -l gitColorStatus (git -c color.status=always status --short 2>/dev/null)
         echo '_fzf_search_git_status: Not in a git repository.' >&2
-    else
-        if set -l selectedPaths (printf '%s\n' $gitColorStatus | fzf --ansi --multi)
-            # git status --short automatically escapes the paths of most files for us so not going to bother trying to handle
-            # the few edges cases of weird file names that should be extremely rare (e.g. "this;needs;escaping")
-            for path in $selectedPaths
-                if test (string sub --length 1 $path) = 'R'
-                    # path has been renamed and looks like "R LICENSE -> LICENSE.md"
-                    # extract the path to use from after the arrow
-                    set cleanedPath (string split -- "-> " $path)[-1]
-                else
-                    set cleanedPath (string sub --start=4 $path)
-                end
-                commandline --insert "$cleanedPath " # add a space after each path to keep them separated when inserted
+    else if set -l selectedPaths (printf '%s\n' $gitColorStatus | fzf --ansi --multi)
+        # git status --short automatically escapes the paths of most files for us so not going to bother trying to handle
+        # the few edges cases of weird file names that should be extremely rare (e.g. "this;needs;escaping")
+        for path in $selectedPaths
+            if test (string sub --length 1 $path) = 'R'
+                # path has been renamed and looks like "R LICENSE -> LICENSE.md"
+                # extract the path to use from after the arrow
+                set cleanedPath (string split -- "-> " $path)[-1]
+            else
+                set cleanedPath (string sub --start=4 $path)
             end
+            commandline --insert $cleanedPath' ' # add a space after each path to keep them separated when inserted
         end
-
-        commandline --function repaint
     end
+
+    commandline --function repaint
 end
