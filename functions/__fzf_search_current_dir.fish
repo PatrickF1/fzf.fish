af3665a508337669069a86ec4d65c6b4d22edcec
Patrick Fong <patrickf3139@gmail.com>
2020-10-24 22:30:14 -0700

    got it working

--- functions/__fzf_search_current_dir.fish
+++ functions/__fzf_search_current_dir.fish
@@ -3,13 +3,16 @@ function __fzf_search_current_dir --description "Search the current directory us
     # Make sure that fzf uses fish to execute __fzf_preview_file.
     # See similar comment in __fzf_search_shell_variables.fish.
     set --local --export SHELL (command --search fish)
-    set file_path_selected (
+    set file_paths_selected (
         fd --hidden --follow --color=always --exclude=.git 2>/dev/null |
-        fzf --ansi --preview='__fzf_preview_file {}'
+        fzf --multi --ansi --preview='__fzf_preview_file {}'
     )
 
     if test $status -eq 0
-        commandline --insert (string escape "$file_path_selected")
+        for path in $file_paths_selected
+            set escaped_path (string escape "$path")
+            commandline --insert "$escaped_path "
+        end
     end
 
     commandline --function repaint
