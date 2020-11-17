33538d9710b0d3987c9e26f5d33c1459f6e6d02a
Jorge Bucaran <mail@jorgebucaran.com>
2020-11-18 02:00:59 +0900

    Use fzf_uninstall event to erase bindings (#58)

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -28,3 +28,19 @@ if not set --query FZF_DEFAULT_OPTS
     # preview-window wrap wraps long lines in the preview window, making reading easier
     set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height 90% --preview-window=wrap'
 end
+
+function _fzf_uninstall --on-event fzf_uninstall
+    # Not going to erase FZF_DEFAULT_OPTS because too hard to tell if it was set by the user or by this plugin
+    if not set --query fzf_fish_custom_keybindings
+        bind --erase --all \cf
+        bind --erase --all \cr
+        bind --erase --all \cv
+        bind --erase --all \e\cl
+        bind --erase --all \e\cs
+
+        set_color --italics cyan
+        echo "fzf.fish key bindings removed"
+        set_color normal
+    end
+    functions --erase _fzf_uninstall
+end
