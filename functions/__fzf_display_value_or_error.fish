38de18e97e4cf7ccf8eaef388a8f7089bcfbc667
Kid <44045911+kidonng@users.noreply.github.com>
2021-01-01 10:49:18 +0800

    Use long flag name

--- functions/__fzf_display_value_or_error.fish
+++ functions/__fzf_display_value_or_error.fish
@@ -1,7 +1,7 @@
 # helper function for __fzf_search_shell_variables
 function __fzf_display_value_or_error --argument-names variable_name --description "Displays either the value of the variable passed in, or an informative message if it is not available."
     if set --query $variable_name
-        set -S $variable_name | string replace -r "^\\\$$variable_name(: )?" ""
+        set --show $variable_name | string replace --regex "^\\\$$variable_name(: )?" ""
     else
         set_color red
         echo "$variable_name was not exported to this process so its value cannot be displayed." >&2
