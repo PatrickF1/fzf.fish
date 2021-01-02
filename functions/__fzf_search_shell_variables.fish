2349eea00732386d52e15dbaeced28bf60918e35
Kid <kidonng@gmail.com>
2021-01-02 17:07:44 +0800

    Remove unnecessary SHELL variable

--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -1,9 +1,4 @@
 function __fzf_search_shell_variables --description "Search and inspect shell variables using fzf. Insert the selected variable into the commandline at the cursor."
-    # Make sure that fzf uses fish to execute __echo_value_or_print_message, which
-    # is an autoloaded fish function so doesn't exist in other shells.
-    # Using --local so that it does not clobber SHELL outside of this function.
-    set --local --export SHELL (command --search fish)
-
     # Pipe the names of all shell variables to fzf and attempt to display the value
     # of the selected variable in fzf's preview window.
     # Using string match to filter variables.
