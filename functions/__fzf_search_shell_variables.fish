796571aeee6fad1ec6c3fff3de0baa394eea3a0e
Kid <kidonng@gmail.com>
2021-01-03 15:58:27 +0800

    Restore __fzf_display_value_or_error

--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -1,14 +1,11 @@
 function __fzf_search_shell_variables --description "Search and inspect shell variables using fzf. Insert the selected variable into the commandline at the cursor."
     # Pipe the names of all shell variables to fzf and attempt to display the value
     # of the selected variable in fzf's preview window.
-    # Using string match to filter variables.
-    # Using string repalace to simplify output.
-    set variable_name (
+    # Non-dumped variables will not be accessible to the fzf process, in which case
+    # __fzf_display_value_or_error will print an informative message in lieu of the value.
+        set variable_name (
         set --names |
-        fzf --preview "cat "(set --show | psub)" |
-                       string match   --regex '^\\\${}(?::|\[).+' |
-                       string replace --regex '^\\\${}(?:: (.+)|(\[.+\]): \|(.+)\|)' '\\\$1\\\$2 \\\$3';
-                       or set_color red && echo \\\${} was not dumped so its value cannot be displayed. >&2 && set_color normal"
+        env SHELL=(command --search fish) fzf --preview '__fzf_display_value_or_error {} '(set --show | psub)
     )
 
     if test $status -eq 0
