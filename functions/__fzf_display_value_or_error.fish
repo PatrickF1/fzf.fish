38fd58571ba149fe04ce4f8c4b6c5c93570a83b8
Kid <kidonng@gmail.com>
2021-01-04 12:01:16 +0800

    Remove cat usage

--- functions/__fzf_display_value_or_error.fish
+++ functions/__fzf_display_value_or_error.fish
@@ -1,12 +1,12 @@
 # helper function for __fzf_search_shell_variables
 function __fzf_display_value_or_error --argument-names variable_name variable_file --description "Displays either the value of the variable passed in, or an informative message if it is not available."
-    set matches (cat $variable_file | string match --regex "^\\\$$variable_name(?::|\[).+")
+    set matches (string match --regex "^\\\$$variable_name(?::|\[).+" <$variable_file)
 
     if test -n "$matches"
         string replace --regex "^\\\$$variable_name(?:: (.+)|(\[.+\]): \|(.+)\|)" '\$1\$2 \$3' $matches
     else
         set_color red
-        echo "$variable_name was not dumped so its value cannot be displayed." >&2
+        echo "$variable_name was not exported so its value cannot be displayed." >&2
         set_color normal
     end
 end
