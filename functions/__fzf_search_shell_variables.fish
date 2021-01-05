cdbc9076947d1c662c28ac40a6e5a6e9a253b4fb
Kid <44045911+kidonng@users.noreply.github.com>
2021-01-05 18:39:12 +0800

    Revert $SHELL

--- functions/__fzf_search_shell_variables.fish
+++ functions/__fzf_search_shell_variables.fish
@@ -1,13 +1,16 @@
 function __fzf_search_shell_variables --description "Search and inspect shell variables using fzf. Insert the selected variable into the commandline at the cursor."
+    # Make sure that fzf uses fish to execute __fzf_display_value_or_error, which
+    # is an autoloaded fish function so doesn't exist in other shells.
+    # Using --local so that it does not clobber SHELL outside of this function.
+    set --local --export SHELL (command --search fish)
+
     # Pipe the names of all shell variables to fzf and attempt to display the value
     # of the selected variable in fzf's preview window.
     # Non-dumped variables will not be accessible to the fzf process, in which case
     # __fzf_display_value_or_error will print an informative message in lieu of the value.
-    # Make sure that fzf uses fish to execute __fzf_display_value_or_error, which
-    # is an autoloaded fish function so doesn't exist in other shells.
     set variable_name (
         set --names |
-        SHELL=$__fish_bin_dir/fish fzf --preview '__fzf_display_value_or_error {} '(set --show | psub)
+        fzf --preview '__fzf_display_value_or_error {} '(set --show | psub)
     )
 
     if test $status -eq 0
