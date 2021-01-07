90197def3dd77cb286d1616f4d29eba1b0416ae8
Kid <kidonng@gmail.com>
2021-01-07 18:12:01 +0800

    Allow quick cd with pre-populated query

--- functions/__fzf_search_current_dir.fish
+++ functions/__fzf_search_current_dir.fish
@@ -11,7 +11,13 @@ function __fzf_search_current_dir --description "Search the current directory us
         # If this function was triggered with an empty commandline and the only thing selected is a directory, then
         # prepend ./ to the dir path. Because fish will attempt to cd implicitly if a directory name starting with a dot
         # is provided, this allows the user to hit Enter one more time to quickly cd into the selected directory.
-        if test (count (commandline --tokenize)) = 0 && test (count $file_paths_selected) = 1 && test -d $file_paths_selected
+        set first_token_length (string length (commandline --tokenize)[1])
+
+        if test -z "$first_token_length"
+            set first_token_length 0
+        end
+
+        if test (string length (commandline)) = $first_token_length && test (count $file_paths_selected) = 1 && test -d $file_paths_selected
             set file_paths_selected ./$file_paths_selected
         end
 
