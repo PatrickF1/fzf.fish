978240dca2338d8140f1c857be5d8f9a8802796a
Patrick <patrickf3139@gmail.com>
2021-01-07 11:48:00 -0800

    update argument name

--- functions/__fzf_filter_shell_variables.fish
+++ functions/__fzf_filter_shell_variables.fish
@@ -1,17 +1,16 @@
-function __fzf_filter_shell_variables --argument-names variable_name variable_values --description "Filter a list of variable values with a variable name."
+function __fzf_filter_shell_variables --argument-names variable_name set_show_output --description "Filter a list of variable values with a variable name."
     # The matches must begin with one of the following two conditions:
     # $variable_name: set
     # $variable_name[
-    string match --entire --regex "^\\\$$variable_name(?:: set|\[)" <$variable_values |
+    string match --entire --regex "^\\\$$variable_name(?:: set|\[)" <$set_show_output |
     # Strip the variable name from the variable info, replacing...
     # $variable_name: set in global scope
     # ...with...
     # set in global scope
-    string replace        --regex "^\\\$$variable_name: " '' |
+    string replace --regex "^\\\$$variable_name: " '' |
     # Strip the variable name from the variable values, replacing...
     # $variable_name[1]: length=14 value=|variable_value|
     # ...with...
     # [1] variable_value
-    string replace        --regex "^\\\$$variable_name(\[.+\]).+\|(.+)\|\$" '\$1 \$2'
+    string replace --regex "^\\\$$variable_name(\[.+\]).+\|(.+)\|\$" '\$1 \$2'
 end
-
