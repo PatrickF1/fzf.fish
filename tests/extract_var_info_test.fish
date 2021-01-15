8300914cba79eed83664e5209574a87e980d753f
Patrick Fong <patrickf3139@gmail.com>
2021-01-15 11:40:24 -0800

    make more readable

--- tests/extract_var_info_test.fish
+++ tests/extract_var_info_test.fish
@@ -1,6 +1,7 @@
 @echo === extract_var_info ===
 
 set --local variable "| a | b | c | d |"
-set --local output (__fzf_extract_var_info variable (set --show | psub) | string collect)
-@test "vars containing |" "$output" = "set in local scope, unexported, with 1 elements
+set --local actual (__fzf_extract_var_info variable (set --show | psub) | string collect)
+set --local expected "set in local scope, unexported, with 1 elements
 [1] | a | b | c | d |"
+@test "vars containing '|'" "$actual" = $expected
