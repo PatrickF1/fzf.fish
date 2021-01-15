b071729b0917d45594433a7226e3205eda89cdc9
Patrick Fong <patrickf3139@gmail.com>
2021-01-15 11:42:59 -0800

    don't need surrounding quotes

--- tests/extract_var_info_test.fish
+++ tests/extract_var_info_test.fish
@@ -4,4 +4,4 @@ set --local variable "| a | b | c | d |"
 set --local actual (__fzf_extract_var_info variable (set --show | psub) | string collect)
 set --local expected "set in local scope, unexported, with 1 elements
 [1] | a | b | c | d |"
-@test "vars containing '|'" "$actual" = $expected
+@test "vars containing '|'" $actual = $expected
