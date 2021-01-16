0712e98c6b09e0f1e821a6e98ef3c69ae2f80c99
Patrick Fong <patrickf3139@gmail.com>
2021-01-15 16:26:40 -0800

    don't hardcode the command, grab it from bindings

--- tests/search_shell_variables.fish
+++ tests/search_shell_variables.fish
@@ -4,6 +4,9 @@ set --local a_local_variable
 set --local --export --append FZF_DEFAULT_OPTS "--filter=a_local_variable"
 mock commandline "--current-token --replace" "echo \$argv" # instead of updating commandline with the result, just output it
 mock commandline \* "" # mock everything else to do nothing
-set --local actual (__fzf_search_shell_variables (set --show | psub) (set --names | psub))
+# grab the command used to execute search shell directly from the bindings
+set --local search_vars_binding (bind \cv)[2]
+set --local search_vars_cmd (echo $search_vars_binding | cut -d" " -f 3- | string unescape)
+set --local actual (eval $search_vars_cmd)
 
 @test "searches local variables" $actual = a_local_variable
