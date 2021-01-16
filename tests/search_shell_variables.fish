47aa917a8ec637c73fc354565bb3702b5b39873f
Patrick Fong <patrickf3139@gmail.com>
2021-01-15 16:12:51 -0800

    comment

--- tests/search_shell_variables.fish
+++ tests/search_shell_variables.fish
@@ -2,7 +2,8 @@
 
 set --local a_local_variable
 set --local --export --append FZF_DEFAULT_OPTS "--filter=a_local_variable"
-mock commandline \* "echo \$argv"
+mock commandline "--current-token --replace" "echo \$argv" # instead of updating commandline with the result, just output it
+mock commandline \* "" # mock everything else to do nothing
 set --local actual (__fzf_search_shell_variables (set --show | psub) (set --names | psub))
 
 @test "searches local variables" $actual = a_local_variable
