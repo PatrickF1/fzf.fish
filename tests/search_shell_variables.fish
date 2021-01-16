f2b0fad322fe5d751b55a4504ac58e256a663f79
Patrick Fong <patrickf3139@gmail.com>
2021-01-15 16:52:58 -0800

    got bindings working

--- tests/search_shell_variables.fish
+++ tests/search_shell_variables.fish
@@ -2,11 +2,10 @@
 
 set --local a_local_variable
 set --local --export --append FZF_DEFAULT_OPTS "--filter=a_local_variable"
-mock commandline "--current-token --replace" "echo \$argv" # instead of updating commandline with the result, just output it
-mock commandline \* "" # mock everything else to do nothing
+mock commandline "--current-token --replace" "echo done" # instead of updating commandline with the result, just output it
+mock commandline \* "echo test" # mock everything else to do nothing
 # grab the command used to execute search shell directly from the bindings
-set --local search_vars_binding (bind \cv)[2]
+set --local search_vars_binding (bind --user \cv)
 set --local search_vars_cmd (echo $search_vars_binding | cut -d" " -f 3- | string unescape)
 set --local actual (eval $search_vars_cmd)
-
 @test "searches local variables" $actual = a_local_variable
