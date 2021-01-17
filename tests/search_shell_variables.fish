5dfab13c261a5e6866de1b32c3708113bf316e15
Patrick Fong <patrickf3139@gmail.com>
2021-01-16 20:50:37 -0800

    save work on new test, waiting for bugfix in clownfish

--- tests/search_shell_variables.fish
+++ tests/search_shell_variables.fish
@@ -12,5 +12,10 @@ set --erase a_local_variable
 set --erase --local FZF_DEFAULT_OPTS
 
 mock fzf \* "" # do nothing if we reach fzf so we don't get stuck
-__fzf_search_shell_variables
-@test "fails if no arguments given" $status -eq 1
+__fzf_search_shell_variables 2>/dev/null
+@test "fails if no arguments given" $status -ne 0
+
+mock commandline --current-token "echo \\\$variable"
+mock fzf \* "echo selection"
+set actual (eval $search_vars_cmd)
+@test "doesn't overwrite \$ when replacing current token with selected variable" $actual = "\$selection"
