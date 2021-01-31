bac0b8567469338008ef30e0d7e62f3b1fe1c975
Patrick Fong <patrickf3139@gmail.com>
2021-01-31 13:18:10 -0800

    don't need to make all vars local

--- tests/search_shell_variables/local_variables.fish
+++ tests/search_shell_variables/local_variables.fish
@@ -1,8 +1,8 @@
 set --local a_local_variable
-set --local --export --append FZF_DEFAULT_OPTS "--filter=a_local_variable"
+set --export --append FZF_DEFAULT_OPTS "--filter=a_local_variable"
 mock commandline "--current-token --replace" "echo \$argv" # instead of updating commandline with the result, just output it
 mock commandline \* "" # mock all other commandline executions to do nothing
 # grab the command used to execute search shell directly from the bindings
-set --local search_vars_cmd (bind --user \cv | cut -d" " -f 3- | string unescape)
-set --local actual (eval $search_vars_cmd)
+set search_vars_cmd (bind --user \cv | cut -d" " -f 3- | string unescape)
+set actual (eval $search_vars_cmd)
 @test "searches local variables" $actual = a_local_variable
