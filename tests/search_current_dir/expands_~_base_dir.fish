d1381d9df24d4512edf0cbfeb8f8d6949b63c4f1
Patrick Fong <patrickf3139@gmail.com>
2021-01-31 16:51:17 -0800

    just refactor tests for now

--- tests/search_current_dir/expands_~_base_dir.fish
+++
@@ -1,7 +0,0 @@
-# mock everything so that the only thing that gets sent to stdout are the args passed into fd
-mock fd \* "echo \$argv"
-mock fzf \* ""
-mock commandline --current-token "echo ~/"
-mock commandline \* ""
-set fd_args (__fzf_search_current_dir)
-@test "~/ is expanded to $HOME" -z (string match --invert "~" $fd_args)
