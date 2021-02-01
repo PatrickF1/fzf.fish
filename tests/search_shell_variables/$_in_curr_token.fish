6c558feee95c34ce82ded8e08d98f5f73d0f9b97
Patrick <patrickf3139@gmail.com>
2021-01-31 17:07:11 -0800

    Refactor: one folder per test suite, one file per test case (#113)
    Put each test in its own file. From fishtape docs
    > Each test file runs inside its own shell, so you can modify the global environment without cluttering your session or breaking other tests
    
    Stopped making all the variables local, since I realized they won't pollute the session that is running the tests.
    Replaced fish version badge with build status badge.

---
+++ tests/search_shell_variables/$_in_curr_token.fish
@@ -0,0 +1,7 @@
+mock commandline \* "" # mock all other commandline executions to do nothing
+mock commandline --current-token "echo \\\$variable"
+mock commandline "--current-token --replace" "echo \$argv" # instead of updating commandline with the result, just output it
+mock fzf \* "echo selection"
+
+set actual (eval $__fzf_search_vars_cmd)
+@test "doesn't overwrite \$ when replacing current token with selected variable" $actual = "\$selection"
