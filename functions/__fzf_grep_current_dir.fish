c6f9fbc975423cc690dd7740706b5bf2fe392845
Ovv <contact@ovv.dev>
2021-02-09 21:31:23 +0100

    rework with bat as preview

--- functions/__fzf_grep_current_dir.fish
+++ functions/__fzf_grep_current_dir.fish
@@ -1,22 +1,20 @@
 function __fzf_grep_current_dir --description "Search current directory content for current token. Replace the current token with the selected file paths."
-    # Make sure that fzf uses fish.
+    # Make sure that fzf uses fish as shell.
     set --local --export SHELL (command --search fish)
 
-    set rg_cmd rg --ignore-case --files-with-matches
-    set redirect "< /dev/tty > /dev/tty 2>&1"
-
+    set rg_cmd rg --color=always --line-number --smart-case --no-heading
     set token (commandline --current-token | string unescape)
 
-    set fzf_arguments --multi --exact --disabled --query=$token \
-        --preview='rg --smart-case --pretty --context 10 {q} {}' \
-        # reload the search when the query cange
-        --bind "change:reload:$rg_cmd {q} || true" \
-        # launch vim on ctrl-e, if there is a query start a search in vim
-        --bind "ctrl-e:execute(if test {q}; vim -c "/{q}" {} $redirect; else; vim {} $redirect; end)" \
-        # launch less on ctrl-v, if there is a query start a search in less
-        --bind "ctrl-v:execute(if test {q}; less -i -p "{q}" {} $redirect; else; less {} $redirect; end)" \
+    set fzf_arguments --multi --ansi --delimiter=: --with-nth=1,3 --disabled --query=$token \
+        --preview 'bat --style=numbers --color=always --highlight-line {2} {1}' \
+        # center the preview window on the highlighted line
+        --preview-window +{2}-/2 \
+        # reload the search when the query change
+        --bind "change:reload:$rg_cmd {q} || true"
 
-    set file_paths_selected ($rg_cmd $token | fzf $fzf_arguments)
+    for file in ($rg_cmd $token | fzf $fzf_arguments)
+        set file_paths_selected $file_paths_selected (string split : $file)[1]
+    end
 
     if test $status -eq 0
         commandline --current-token --replace -- (string escape -- $file_paths_selected | string join ' ')
