8a170f9ed9e51c02ef453dda28ef2a0057ad7580
Patrick Fong <patrickf3139@gmail.com>
2021-02-15 09:29:38 -0800

    make fd command configurable

--- tests/search_current_dir/no_base_dir_if_no_slash.fish
+++ tests/search_current_dir/no_base_dir_if_no_slash.fish
@@ -1,6 +1,6 @@
 set --export fd_args
 function fd
-    set fd_args $argv
+    set fd_opts $argv
 end
 mock fzf \* ""
 mock commandline --current-token "echo functions"
@@ -8,7 +8,7 @@ mock commandline "--current-token --replace" ""
 mock commandline \* ""
 __fzf_search_current_dir
 if test -d functions
-    @test "doesn't change fd's base directory if no slash on current token" -z (string match --entire -- "--base-directory" $fd_args)
+    @test "doesn't change fd's base directory if no slash on current token" -z (string match --entire -- "--base-directory" $fd_opts)
 else
     @test "functions/ exists for testing purposes"
 end
