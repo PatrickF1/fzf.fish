f14a1d38a6c766184173b5f6c5b6db9750744113
Patrick Fong <patrickf3139@gmail.com>
2021-02-15 21:36:15 -0800

    Remove leading underscores from fzf_search_vars_cmd
    Since fzf_search_vars_cmd is meant to be exposed and used by the user (for users who customize their key bindings), it should not start with __, which is conventionally used to signal that the entity should be off limits to the user.

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -2,14 +2,14 @@
 # them before even executing __fzf_search_shell_variables. We use psub to store the
 # variables' info in temporary files and pass in the filenames as arguments.
 # This variable is intentionally global so that it can be referenced by custom key bindings and tests
-set --global __fzf_search_vars_cmd '__fzf_search_shell_variables (set --show | psub) (set --names | psub)'
+set --global fzf_search_vars_cmd '__fzf_search_shell_variables (set --show | psub) (set --names | psub)'
 
 # Set up the default, mnemonic key bindings unless the user has chosen to customize them
 if not set --query fzf_fish_custom_keybindings
     # \cf is Ctrl+f
     bind \cf '__fzf_search_current_dir'
     bind \cr '__fzf_search_history'
-    bind \cv $__fzf_search_vars_cmd
+    bind \cv $fzf_search_vars_cmd
     # The following two key binding use Alt as an additional modifier key to avoid conflicts
     bind \e\cl '__fzf_search_git_log'
     bind \e\cs '__fzf_search_git_status'
@@ -18,7 +18,7 @@ if not set --query fzf_fish_custom_keybindings
     if test "$fish_key_bindings" = 'fish_vi_key_bindings'
         bind --mode insert \cf '__fzf_search_current_dir'
         bind --mode insert \cr '__fzf_search_history'
-        bind --mode insert \cv $__fzf_search_vars_cmd
+        bind --mode insert \cv $fzf_search_vars_cmd
         bind --mode insert \e\cl '__fzf_search_git_log'
         bind --mode insert \e\cs '__fzf_search_git_status'
     end
