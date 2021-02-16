d3e61860853e195de1a7b711f42648c914e9cb90
Patrick Fong <patrickf3139@gmail.com>
2021-02-15 17:07:35 -0800

    add a test

--- tests/search_current_dir/no_base_dir_if_no_slash.fish
+++ tests/search_current_dir/no_base_dir_if_no_slash.fish
@@ -1,6 +1,6 @@
-set --export fd_args
+set --export fd_captured_args "overwrite me"
 function fd
-    set fd_opts $argv
+    set fd_captured_args $argv
 end
 mock fzf \* ""
 mock commandline --current-token "echo functions"
@@ -8,7 +8,7 @@ mock commandline "--current-token --replace" ""
 mock commandline \* ""
 __fzf_search_current_dir
 if test -d functions
-    @test "doesn't change fd's base directory if no slash on current token" -z (string match --entire -- "--base-directory" $fd_opts)
+    @test "doesn't change fd's base directory if no slash on current token" -z (string match --entire -- "--base-directory" $fd_captured_args)
 else
-    @test "functions/ exists for testing purposes"
+    @test "functions/ doesn't exists for testing purposes"
 end
