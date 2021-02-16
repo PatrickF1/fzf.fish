d72dbddccc51bbc95383a117197bf555e5532f88
Patrick Fong <patrickf3139@gmail.com>
2021-02-15 19:58:31 -0800

    make no_base_dir_if_no_slash fail if there is actually a bug

--- tests/search_current_dir/no_base_dir_if_no_slash.fish
+++ tests/search_current_dir/no_base_dir_if_no_slash.fish
@@ -1,4 +1,4 @@
-set --export fd_captured_args "overwrite me"
+set --export fd_captured_args
 function fd
     set fd_captured_args $argv
 end
@@ -7,8 +7,10 @@ mock commandline --current-token "echo functions"
 mock commandline "--current-token --replace" ""
 mock commandline \* ""
 __fzf_search_current_dir
+
 if test -d functions
-    @test "doesn't change fd's base directory if no slash on current token" -z (string match --entire -- "--base-directory" $fd_captured_args)
+    test -n "$fd_captured_args" && test -z (string match --entire -- "--base-directory" $fd_captured_args)
+    @test "doesn't change fd's base directory if no slash on current token" $status -eq 0
 else
     @test "functions/ doesn't exists for testing purposes"
 end
