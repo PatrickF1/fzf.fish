231ec290f9a5bb3cef42b3cf6d03909affca0ce1
Kid <kidonng@gmail.com>
2021-03-09 19:44:50 +0800

    🤷‍♀️

--- tests/preview_file/custom_dir_preview.fish
+++ tests/preview_file/custom_dir_preview.fish
@@ -1,4 +1,5 @@
 set fzf_preview_dir_cmd ls
-touch file.txt .hidden
-set actual (__fzf_preview_file .)
+set dir (mkdtemp -d)
+touch $dir/{file.txt,.hidden}
+set actual (__fzf_preview_file $dir)
 @test "correctly uses custom command to preview directories" "$actual" = file.txt
