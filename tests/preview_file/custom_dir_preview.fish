b87a24b33e20b27ddb5bdee97a2094bba152573f
Kid <kidonng@gmail.com>
2021-03-09 19:51:52 +0800

    fix typo

--- tests/preview_file/custom_dir_preview.fish
+++ tests/preview_file/custom_dir_preview.fish
@@ -1,5 +1,5 @@
 set fzf_preview_dir_cmd ls
-set dir (mkdtemp -d)
+set dir (mktemp -d)
 touch $dir/{file.txt,.hidden}
 set actual (__fzf_preview_file $dir)
 @test "correctly uses custom command to preview directories" "$actual" = file.txt
