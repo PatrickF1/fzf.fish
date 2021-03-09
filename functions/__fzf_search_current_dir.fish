b6b60bdf818d6ba0455da5e3691de5d9d00f3545
Kid <kidonng@gmail.com>
2021-03-09 18:49:00 +0800

    Fix escaping

--- functions/__fzf_search_current_dir.fish
+++ functions/__fzf_search_current_dir.fish
@@ -8,14 +8,15 @@ function __fzf_search_current_dir --description "Search the current directory. R
     set current_token (commandline --current-token)
     set token (string unescape -- $current_token)
     # expand the token (for tilde, variables, etc.) into full path
-    set expanded_token (eval "printf -- $token")
+    set expanded_token (eval "echo -- $token")
+    set escaped_expanded_token (string escape -- $expanded_token)
 
     # If the current token is a directory and has a trailing slash,
     # then use it as fd's base directory.
     if string match --quiet -- "*/" $token && test -d $expanded_token
         set --append fd_opts --base-directory=$expanded_token
         # use the directory name as fzf's prompt to indicate the search is limited to that directory
-        set --append fzf_arguments --prompt=$token --preview="__fzf_preview_file $expanded_token{}"
+        set --append fzf_arguments --prompt=$token --preview="__fzf_preview_file $escaped_expanded_token{}"
         set file_paths_selected $expanded_token(fd $fd_opts 2>/dev/null | fzf $fzf_arguments)
     else
         set --append fzf_arguments --query=$token --preview='__fzf_preview_file {}'
