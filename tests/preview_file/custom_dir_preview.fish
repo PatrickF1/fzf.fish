ecbe926fd89889d720740a495c47a12c56f880fd
Kid <kidonng@gmail.com>
2021-03-09 19:40:41 +0800

    Try to fix test

--- tests/preview_file/custom_dir_preview.fish
+++ tests/preview_file/custom_dir_preview.fish
@@ -1,4 +1,4 @@
-set fzf_preview_dir_cmd exa
-mock exa . "echo file.txt"
+set fzf_preview_dir_cmd ls
+touch file.txt .hidden
 set actual (__fzf_preview_file .)
 @test "correctly uses custom command to preview directories" "$actual" = file.txt
