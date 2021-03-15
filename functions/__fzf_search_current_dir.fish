80d3dce3eaf8956012b44f76b1ce5bdbe80e55a0
Patrick Fong <patrickf3139@gmail.com>
2021-03-14 17:25:30 -0700

    fix fzf prompt as well

--- functions/__fzf_search_current_dir.fish
+++ functions/__fzf_search_current_dir.fish
@@ -17,7 +17,7 @@ function __fzf_search_current_dir --description "Search the current directory. R
     if string match --quiet -- "*/" $token && test -d $expanded_token
         set --append fd_opts --base-directory=$expanded_token
         # use the directory name as fzf's prompt to indicate the search is limited to that directory
-        set --append fzf_arguments --prompt=$expanded_token --preview="__fzf_preview_file $expanded_token{}"
+        set --append fzf_arguments --prompt=$unescaped_token --preview="__fzf_preview_file $expanded_token{}"
         set file_paths_selected $expanded_token(fd $fd_opts 2>/dev/null | fzf $fzf_arguments)
     else
         set --append fzf_arguments --query=$unescaped_token --preview='__fzf_preview_file {}'
