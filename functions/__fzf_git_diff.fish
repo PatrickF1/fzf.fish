17180b53fec0f30ceb3a5dd2d049f8cd1e893166
Kid <kidonng@gmail.com>
2021-03-17 23:44:26 +0800

    Add git diff preview to Modified paths function

---
+++ functions/__fzf_git_diff.fish
@@ -0,0 +1,7 @@
+function __fzf_git_diff --argument-names file_status file_path --description "Print git diff for the given file based on its status."
+    if test $file_status = "A" || test $file_status = "??"
+        git diff --color=always --no-index -- /dev/null $file_path
+    else
+        git diff --color=always -- $file_path
+    end
+end
