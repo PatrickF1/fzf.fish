2293bd9833ade839cf5ce0503ad26ceb55cbb2c1
Kid <kidonng@gmail.com>
2021-03-17 23:50:46 +0800

    Format

--- functions/__fzf_git_diff.fish
+++ functions/__fzf_git_diff.fish
@@ -1,5 +1,5 @@
 function __fzf_git_diff --argument-names file_status file_path --description "Print git diff for the given file based on its status."
-    if test $file_status = "A" || test $file_status = "??"
+    if test $file_status = A || test $file_status = "??"
         git diff --color=always --no-index -- /dev/null $file_path
     else
         git diff --color=always -- $file_path
