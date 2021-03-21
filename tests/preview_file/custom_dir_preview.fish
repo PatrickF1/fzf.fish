a5d9451563d8d11154ca5b0053987a35d07700c9
Patrick Fong <patrickf3139@gmail.com>
2021-03-21 16:20:14 -0700

    transition to using test resources

--- tests/preview_file/custom_dir_preview.fish
+++ tests/preview_file/custom_dir_preview.fish
@@ -1,9 +1,9 @@
-set multi_word_dir = "multi word dir"
+set multi_word_dir = "tests/_resources/multi word dir"
 
 set fzf_preview_dir_cmd exa
 function exa
     @test "passes file path as a single argument to fzf_preview_dir_cmd" (count $argv) -eq 1
-    echo $argv
+    ls $argv
 end
 set actual (__fzf_preview_file "$multi_word_dir")
-@test "correctly uses custom command to preview directories" "$actual" = "multi word path"
+@test "correctly uses custom command to preview directories" "$actual" = "file 1.txt file2.txt"
