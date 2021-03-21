c1543404af047781f9a573f5b9854afa7f541700
Patrick Fong <patrickf3139@gmail.com>
2021-03-21 11:39:01 -0700

    start work on next test

--- tests/preview_file/custom_dir_preview.fish
+++ tests/preview_file/custom_dir_preview.fish
@@ -1,4 +1,12 @@
+set multi_word_dir = "multi word dir"
+mktemp multi_word_dir
+
 set fzf_preview_dir_cmd exa
-mock exa . "echo file.txt"
-set actual (__fzf_preview_file .)
-@test "correctly uses custom command to preview directories" "$actual" = file.txt
+function exa
+    @test "passes file path as a single argument to fzf_preview_dir_cmd" (count $argv) -eq 1
+    echo $argv
+end
+set actual (__fzf_preview_file "$multi_word_dir")
+@test "correctly uses custom command to preview directories" "$actual" = "multi word path"
+
+rm -rdmulti_word_dir
