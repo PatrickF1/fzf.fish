207a34058cea4b2f833a360234aca2743ed19949
Patrick Fong <patrickf3139@gmail.com>
2021-03-22 09:26:18 -0700

    finish test

--- tests/search_current_dir/base_dir_with_spaces.fish
+++ tests/search_current_dir/base_dir_with_spaces.fish
@@ -1,7 +1,7 @@
-mock commandline --current-token "echo tests/_resources/multi\\ word\\ dir/"
-mock commandline "--current-token --replace" ""
+mock commandline --current-token "echo tests/_resources/multi word dir/"
+mock commandline "--current-token --replace --" "echo \$argv"
 mock commandline \* ""
-set --export --append FZF_DEFAULT_OPTS "--filter=''" # automatically select all
+set --export --append FZF_DEFAULT_OPTS "--filter=''" # automatically select all input lines sent to fzf
 set actual (__fzf_search_current_dir)
-
-@test "uses current token as base directory if it ends in / and is a directory" (count $actual) -eq 2
+set expected "'tests/_resources/multi word dir/file 1.txt' 'tests/_resources/multi word dir/file 2.txt'"
+@test "uses current token as base directory if it ends in / and is a directory" "$actual" = $expected
