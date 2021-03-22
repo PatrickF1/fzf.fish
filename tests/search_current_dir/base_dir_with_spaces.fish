460ae59409788b9aead20f193a2ab513fd41a5a3
Patrick Fong <patrickf3139@gmail.com>
2021-03-22 09:04:14 -0700

    tdd: write failing test first

--- tests/search_current_dir/base_dir_with_spaces.fish
+++ tests/search_current_dir/base_dir_with_spaces.fish
@@ -1,12 +1,7 @@
-set --export fd_captured_opts
-function fd
-    set fd_captured_opts $argv
-end
-mock fzf \* ""
 mock commandline --current-token "echo tests/_resources/multi\\ word\\ dir/"
 mock commandline "--current-token --replace" ""
 mock commandline \* ""
-__fzf_search_current_dir
+set --export --append FZF_DEFAULT_OPTS "--filter=''" # automatically select all
+set actual (__fzf_search_current_dir)
 
-contains -- "--base-directory=tests/_resources/multi word dir/" $fd_captured_opts
-@test "uses current token as base directory if it ends in / and is a directory" $status -eq 0
+@test "uses current token as base directory if it ends in / and is a directory" (count $actual) -eq 2
