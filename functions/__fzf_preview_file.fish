43d131f62645dbc8be7b37295e91298a53ed0d8e
Kid <kidonng@gmail.com>
2021-03-23 11:13:48 +0800

    Merge

--- functions/__fzf_preview_file.fish
+++ functions/__fzf_preview_file.fish
@@ -4,6 +4,7 @@ function __fzf_preview_file --argument-names file_path --description "Print a pr
         bat --style=numbers --color=always "$file_path"
     else if test -d "$file_path" # directory
         if set --query fzf_preview_dir_cmd
+            # need to escape quotes to make sure eval receives file_path as a single arg
             eval $fzf_preview_dir_cmd \"$file_path\"
         else
             # -A list hidden files as well, except for . and ..
