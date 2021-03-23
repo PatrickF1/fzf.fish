62b26e194f880ab6b46051a82d90b450b6c88247
Patrick Fong <patrickf3139@gmail.com>
2021-03-22 23:51:08 -0700

    work on new test

--- tests/search_current_dir/expands_vars.fish
+++ tests/search_current_dir/expands_vars.fish
@@ -1,9 +1,9 @@
 set target "file 1.txt"
 
-set --global --export fzf_dir_opts --filter=""
+set --global fzf_dir_opts --select-1
 mock commandline --current-token "echo \$target"
-mock commandline "--current-token --replace" "echo \$argv"
+mock commandline "--current-token --replace --" "echo \$argv"
 mock commandline \* ""
 
 set actual (__fzf_search_current_dir)
-@test "expands variables in current token" "$actual" = "file 1.txt"
+@test "expands variables in current token" (basename "$actual") = "file 1.txt"
