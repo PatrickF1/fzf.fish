5e94ee0bef0c4901e24876945b1c81536e2d6715
Patrick Fong <patrickf3139@gmail.com>
2021-03-26 22:10:36 -0700

    simplify

--- functions/__fzf_extract_var_info.fish
+++ functions/__fzf_extract_var_info.fish
@@ -1,18 +1,15 @@
 # helper function for __fzf_search_shell_variables
 function __fzf_extract_var_info --argument-names variable_name set_show_output --description "Extract and reformat lines pertaining to \$variable_name from \$set_show_output."
-    # Extract only the lines about the variable, all of which begin with
-    # $variable_name: or $variable_name[
+    # Extract only the lines about the variable, all of which begin with either
+    # $variable_name: ...or... $variable_name[
     string match --regex "^\\\$$variable_name(?::|\[).*" <$set_show_output |
 
-        # Strip the variable name from the scope info, replacing...
-        #   $variable_name: set in global scope
-        # ...with...
-        #   set in global scope
-        string replace --regex "^\\\$$variable_name: " '' |
+        # Strip the variable name prefix, including ": " for scope info lines
+        string replace --regex "^\\\$$variable_name(?:: )?" '' |
 
-        # From the lines of values, keep only the index and value, replacing...
-        #   $variable_name[1]: |value|
+        # Refine the lines of values, replacing...
+        #   [1]: |value|
         # ...with...
         #   [1] value
-        string replace --regex "^\\\$$variable_name(\[\d+\]): \|(.+)\|" '\$1 \$2'
+        string replace --regex ": \|(.*)\|" ' \$1'
 end
