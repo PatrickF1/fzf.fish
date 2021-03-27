59570ecd39526dde5ae37f3be2abb2c1ab24e407
Patrick Fong <patrickf3139@gmail.com>
2021-03-26 19:58:19 -0700

    update comments

--- functions/__fzf_extract_var_info.fish
+++ functions/__fzf_extract_var_info.fish
@@ -1,18 +1,20 @@
 # helper function for __fzf_search_shell_variables
 function __fzf_extract_var_info --argument-names variable_name set_show_output --description "Extract and reformat lines pertaining to \$variable_name from \$set_show_output."
-    # Extract only the lines about the variable
+    # Extract only the lines about the variable, all of which begin with $variable_name
     string match --regex "^\\\$$variable_name.*" <$set_show_output |
 
         # Strip the variable name from the scope info, replacing...
-        # $variable_name: set in global scope
+        #   $variable_name: set in global scope
+        #   $variable_name[1]: | value |
         # ...with...
-        # set in global scope
+        #   set in global scope
+        #   [1]: value
         string replace --regex "^\\\$$variable_name: " '' |
 
         # From the lines of values, keep only the index and value, replacing...
-        # $variable_name[1]: length=14 value=|variable_value|
+        #   $variable_name[1]: length=14 value=|variable_value|
         # ...with...
-        # [1] variable_value
+        #   [1] variable_value
         string replace --regex "^\\\$$variable_name(\[\d+\]).+?\|(.+)\|\$" '\$1 \$2'
 
     # Final output example for $PATH:
