79d98eeec9e5c87aae1230040fabd26c3a3ba3f1
Patrick Fong <patrickf3139@gmail.com>
2021-05-09 21:49:57 -0700

    no longer need to qualify fzf_search_vars_cmd

--- functions/fzf_install_bindings.fish
+++ functions/fzf_install_bindings.fish
@@ -2,13 +2,13 @@ function fzf_set_up_bindings -a dir git_log git_status command_history shell_var
     # Because of scoping rules, to capture the shell variables exactly as they are, we must read
     # them before even executing __fzf_search_shell_variables. We use psub to store the
     # variables' info in temporary files and pass in the filenames as arguments.
-    set --local fzf_search_vars_cmd '__fzf_search_shell_variables (set --show | psub) (set --names | psub)'
+    set --local search_vars_command '__fzf_search_shell_variables (set --show | psub) (set --names | psub)'
 
     bind $dir __fzf_search_current_dir
     bind $git_log __fzf_search_git_log
     bind $git_status __fzf_search_git_status
     bind $command_history __fzf_search_history
-    bind $shell_vars $fzf_search_vars_cmd
+    bind $shell_vars $search_vars_command
 
     # set up the same key bindings for insert mode if using insert mode
     if contains "$fish_key_bindings" fish_vi_key_bindings fish_hybrid_key_bindings
@@ -16,6 +16,6 @@ function fzf_set_up_bindings -a dir git_log git_status command_history shell_var
         bind --mode insert $git_log __fzf_search_git_log
         bind --mode insert $git_status __fzf_search_git_status
         bind --mode insert $command_history __fzf_search_history
-        bind --mode insert $shell_vars $fzf_search_vars_cmd
+        bind --mode insert $shell_vars $search_vars_command
     end
 end
