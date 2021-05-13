02f21ab0786722e7a79d91b90170a9a6f4025e5c
Kid <44045911+kidonng@users.noreply.github.com>
2021-05-13 22:37:57 +0800

    Format

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -15,8 +15,8 @@ if not set --query fzf_fish_custom_keybindings
             # The following two key binding use Alt as an additional modifier key to avoid conflicts
             bind \e\cl __fzf_search_git_log
             bind \e\cs __fzf_search_git_status
-        # set up the same key bindings for insert mode if not using fish_default_key_bindings
         else
+            # set up the same key bindings for insert mode if not using fish_default_key_bindings
             bind --mode insert \cf __fzf_search_current_dir
             bind --mode insert \cr __fzf_search_history
             bind --mode insert \cv $fzf_search_vars_cmd
