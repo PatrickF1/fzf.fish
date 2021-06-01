a1f9cae21e6dedb74f8d7b0726c21a63e65a8ff8
Patrick Fong <patrickf3139@gmail.com>
2021-05-31 20:00:27 -0700

    Don't erase all user bindings on uninstall
    bind --erase --all was erasing all user bindings, including any user
    specified bindings NOT installed by fzf.fish. This was unintentional. By specifying a
    mode and not using --all, the command now only erases bindings set by
    fzf.fish by default.
    In addition, I greatly cut lines of code by specifying all sequences in
    one command.

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -39,11 +39,8 @@ end
 function _fzf_uninstall --on-event fzf_uninstall
     # Not going to erase FZF_DEFAULT_OPTS because too hard to tell if it was set by the user or by this plugin
     if not set --query fzf_fish_custom_keybindings
-        bind --erase --all \cf
-        bind --erase --all \cr
-        bind --erase --all \cv
-        bind --erase --all \e\cl
-        bind --erase --all \e\cs
+        bind --erase \cf \cr \cv \e\cl \e\cs
+        bind --erase --mode insert \cf \cr \cv \e\cl \e\cs
 
         set_color --italics cyan
         echo "fzf.fish key bindings removed"
