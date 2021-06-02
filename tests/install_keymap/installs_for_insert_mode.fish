220444a2b51cf244aa1de69d17e01efbc3cdf921
Patrick Fong <patrickf3139@gmail.com>
2021-06-02 11:49:28 -0700

    amend test for insert mode

--- tests/install_keymap/installs_for_insert_mode.fish
+++ tests/install_keymap/installs_for_insert_mode.fish
@@ -1,7 +1,3 @@
-# by adding a binding for insert mode, insert mode is automatically created
-bind --mode insert q ""
-fzf_simple_mnemonic_keymap
-
-# now for __fzf_search_history, there should be one binding for default mode and one for insert mode
-bind --mode insert \cr >/dev/null
-@test "installs insert mode bindings if insert mode" $status -eq 0
+# fzf_conflictless_mnemonic_keymap already when fzf.fish was sourced on shell startup
+bind --mode insert \e\cf && bind --mode insert \cv >/dev/null
+@test "installs insert mode bindings" $status -eq 0
