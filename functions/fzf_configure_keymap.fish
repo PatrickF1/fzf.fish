e8d1bf6fdb112e0322998872436dc75acca6c742
Patrick Fong <patrickf3139@gmail.com>
2021-06-03 14:10:26 -0700

    shorten code using key_sequence

--- functions/fzf_configure_keymap.fish
+++ functions/fzf_configure_keymap.fish
@@ -2,31 +2,17 @@
 # Always installs bindings for insert mode since for simplicity and b/c it has almost no side-effect
 # https://gitter.im/fish-shell/fish-shell?at=60a55915ee77a74d685fa6b1
 function fzf_configure_keymap --argument-names keymap_or_help --description "Change the key bindings for fzf.fish to the specified key sequences."
-    set --local directory
-    set --local git_log
-    set --local git_status
-    set --local history_ # append _ because history is a special variable
-    set --local variables
+    # index 1 = directory, 2 = git_log, 3 = git_status, 4 = history, 5 = variables
+    set --local key_sequences
     switch $keymap_or_help
         case conflictless_mnemonic
-            set directory \e\cf
-            set git_log \e\cl
-            set git_status \cs
-            set history_ \cr
-            set variables \cv
+            set key_sequences \e\cf \e\cl \cs \cr \cv
         case simple_mnemonic
-            set directory \cf
-            set git_log \cl
-            set git_status \cs
-            set history_ \cr
-            set variables \cv
+            set key_sequences \cf \cl \cs \cr \cv
         case simple_conflictless
-            set directory \co
-            set git_log \cl
-            set git_status \cg
-            set history_ \er
-            set variables \ex
+            set key_sequences \co \cl \cg \er \ex
         case blank
+            set key_sequences "" "" "" "" ""
         case -h --help
             _fzf_configure_keymap_help
             return
@@ -43,11 +29,11 @@ function fzf_configure_keymap --argument-names keymap_or_help --description "Cha
         _fzf_configure_keymap_help
         return 22
     else
-        set --query _flag_directory && set directory $_flag_directory
-        set --query _flag_git_log && set git_log $_flag_git_log
-        set --query _flag_git_status && set git_status $_flag_git_status
-        set --query _flag_history && set history_ $_flag_history
-        set --query _flag_variables && set variables $_flag_variables
+        set --query _flag_directory && set key_sequences[1] "$_flag_directory"
+        set --query _flag_git_log && set key_sequences[2] "$_flag_git_log"
+        set --query _flag_git_status && set key_sequences[3] "$_flag_git_status"
+        set --query _flag_history && set key_sequences[4] "$_flag_history"
+        set --query _flag_variables && set key_sequences[5] "$_flag_variables"
     end
 
     # If another keymap already exists, uninstall it first for a clean slate
@@ -55,24 +41,16 @@ function fzf_configure_keymap --argument-names keymap_or_help --description "Cha
         _fzf_uninstall_keymap
     end
 
-    # When no key sequence is provided to bind, it gives an error.
-    # Since this is expected for partial keymnaps, we ignore the stderr from these bind commands.
     for mode in default insert
-        test -n $directory && bind --mode $mode $directory __fzf_search_current_dir
-        test -n $git_log && bind --mode $mode $git_log __fzf_search_git_log
-        test -n $git_status && bind --mode $mode $git_status __fzf_search_git_status
-        test -n $history_ && bind --mode $mode $history_ __fzf_search_history
-        test -n $variables && bind --mode $mode $variables $_fzf_search_vars_command
-    end 2>/dev/null
+        test -n $key_sequences[1] && bind --mode $mode $key_sequences[1] __fzf_search_current_dir
+        test -n $key_sequences[2] && bind --mode $mode $key_sequences[2] __fzf_search_git_log
+        test -n $key_sequences[3] && bind --mode $mode $key_sequences[3] __fzf_search_git_status
+        test -n $key_sequences[4] && bind --mode $mode $key_sequences[4] __fzf_search_history
+        test -n $key_sequences[5] && bind --mode $mode $key_sequences[5] $_fzf_search_vars_command
+    end
 
-    function _fzf_uninstall_keymap \
-        --inherit-variable directory \
-        --inherit-variable git_log \
-        --inherit-variable git_status \
-        --inherit-variable history_ \
-        --inherit-variable variables
-
-        bind --erase -- $directory $git_log $git_status $history_ $variables
-        bind --erase --mode insert -- $directory $git_log $git_status $history_ $variables
+    function _fzf_uninstall_keymap --inherit-variable key_sequences
+        bind --erase -- $key_sequences
+        bind --erase --mode insert -- $key_sequences
     end
 end
