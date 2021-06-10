719667ada33eafdfa683715df5bd2af6dd6d0997
Patrick Fong <patrickf3139@gmail.com>
2021-06-10 11:39:56 -0700

    update test

--- tests/search_shell_variables/$_in_curr_token.fish
+++ tests/search_shell_variables/$_in_curr_token.fish
@@ -1,7 +1,7 @@
 mock commandline \* "" # mock all other commandline executions to do nothing
-mock commandline --current-token "echo \\\$variable"
+mock commandline --current-token "echo \\\$variable" # simulate current token starting with $
 mock commandline "--current-token --replace" "echo \$argv" # instead of updating commandline with the result, just output it
-mock fzf \* "echo selection"
+mock fzf \* "echo a\nb\nc"
 
 set actual (eval $fzf_search_vars_cmd)
-@test "doesn't overwrite \$ when replacing current token with selected variable" "$actual" = "\$selection"
+@test "doesn't overwrite \$ when replacing current token with selected variable" "$actual" = "\$a \$b \$c"
