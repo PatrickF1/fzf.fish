dcf8f4ddaadf4a01f1a5ffa793df31d332001047
Patrick Fong <patrickf3139@gmail.com>
2021-06-10 22:15:10 -0700

    update help

--- functions/_fzf_configure_bindings_help.fish
+++ functions/_fzf_configure_bindings_help.fish
@@ -1,11 +1,11 @@
 function _fzf_configure_bindings_help --description "Prints the help message for fzf_configure_bindings."
     echo "\
 USAGE:
-    fzf_configure_bindings [--SEARCHABLE_ENTITY[=KEY_SEQUENCE]...]
+    fzf_configure_bindings [--ENTITY[=KEY_SEQUENCE]...]
 
 DESCRIPTION
-    fzf_configure_bindings installs mnemonic key bindings for fzf.fish's features, each of which
-    can be customized through a corresponding option:
+    By default, fzf_configure_bindings installs mnemonic key bindings for fzf.fish's features. Each
+    feature's binding can be customized through a corresponding namesake option:
         FEATURE            |  MNEMONIC KEY SEQUENCE        |  CORRESPONDING OPTION
         Search directory   |  Ctrl+Alt+F (F for file)      |  --directory
         Search git log     |  Ctrl+Alt+L (L for log)       |  --git_log
@@ -13,16 +13,17 @@ DESCRIPTION
         Search history     |  Ctrl+H     (H for history)   |  --history
         Search variables   |  Ctrl+V     (V for variable)  |  --variable
     An option with a key sequence value overrides the binding for its feature, while an option
-    without a value disables the binding. Features that are not customized retain their default
-    menomonic bindings specified above. Key bindings are always installed for both default and
-    insert mode. fish_key_reader can help generate key sequences.
+    without a value disables the binding. Options and their values must be separated by an equal
+    sign, not a space. Features that are not customized retain their default menomonic bindings
+    specified above. Key bindings are always installed for default and insert modes. Use
+    fish_key_reader to determine key sequences.
 
     fzf_configure_bindings fails and refuses to install bindings if passed unknown options.
-    However, it does not validate key sequences and installs them as is.
+    However, it does not validate key sequences.
 
     fzf_configure_bindings erases any bindings it previously installed before installing new ones.
-    This means users are free to repeatedly invoke this function in the same fish session without
-    concern of residual bindings or other side effects.
+    This means users may repeatedly invoke it in the same fish session without concern of residual
+    bindings or other side effects.
 
     The -h and --help options print this help message.
 
