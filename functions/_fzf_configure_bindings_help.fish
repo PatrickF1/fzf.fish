5725749559345d92b87e87cef77bf4c12e9f4c58
Patrick Fong <patrickf3139@gmail.com>
2021-06-10 17:21:41 -0700

    update help style

--- functions/_fzf_configure_bindings_help.fish
+++ functions/_fzf_configure_bindings_help.fish
@@ -1,9 +1,9 @@
 function _fzf_configure_bindings_help --description "Prints the help message for fzf_configure_bindings."
     echo "\
-Usage:
+USAGE:
     fzf_configure_bindings [--SEARCHABLE_ENTITY[=KEY_SEQUENCE]...]
 
-Description:
+DESCRIPTION
     fzf_configure_bindings installs mnemonic key bindings for fzf.fish's features, each of which
     can be customized through a corresponding option:
         FEATURE            |  MNEMONIC KEY SEQUENCE        |  CORRESPONDING OPTION
@@ -13,25 +13,27 @@ Description:
         Search history     |  Ctrl+H     (H for history)   |  --history
         Search variables   |  Ctrl+V     (V for variable)  |  --variable
     An option with a key sequence value overrides the binding for its feature, while an option
-    without a value disables the binding. Features that are not customized retain their default,
-    menomonic binding specified above. fish_key_reader can help generate key sequences.
-    Key bindings are always installed for both default and insert mode.
+    without a value disables the binding. Features that are not customized retain their default
+    menomonic bindings specified above. Key bindings are always installed for both default and
+    insert mode. fish_key_reader can help generate key sequences.
 
-    fzf_configure_bindings fails and refuses to install new bindings if passed unknown options.
+    fzf_configure_bindings fails and refuses to install bindings if passed unknown options.
     However, it does not validate key sequences and installs them as is.
 
     fzf_configure_bindings erases any bindings it previously installed before installing new ones.
-    This means users are free to repeatedly invoke this function in the same fish session to
-    experiment with different bindings without having to worry about residual bindings.
+    This means users are free to repeatedly invoke this function in the same fish session without
+    concern of residual bindings or other side effects.
 
     The -h and --help options print this help message.
 
-Examples:
-    Install the default, mnemonic bindings
+EXAMPLES
+    Install the default mnemonic bindings
         \$ fzf_configure_bindings
-    Install the mnemonic bindings but override git log's binding
+    Install the default bindings but override git log's binding to Ctrl+G
         \$ fzf_configure_bindings --git_log=\cg
-    Install the mnemonic bindings but leave search history unbound
+    Install the default bindings but leave search history unbound
         \$ fzf_configure_bindings --history
+    Alternative style of disabling search history
+        \$ fzf_configure_bindings --history=
 "
 end
