eab8afe01634ca4994087d6bf611fcae8ae7ca4e
Patrick Fong <patrickf3139@gmail.com>
2021-06-16 13:31:24 -0700

    start work on completions

---
+++ completions/fzf_configure_bindings.fish
@@ -0,0 +1,7 @@
+complete fzf_configure_bindings --no-files
+complete fzf_configure_bindings --long help --short h --description "Print help"
+complete fzf_configure_bindings --long directory --description "Specify a key sequence for search directory"
+complete fzf_configure_bindings --long git_log --description "Specify a key sequence for search git log"
+complete fzf_configure_bindings --long git_status --description "Specify a key sequence for search git status"
+complete fzf_configure_bindings --long history --description "Specify a key sequence for search history"
+complete fzf_configure_bindings --long variables --description "Specify a key sequence for search variables"
