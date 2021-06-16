277fad5814e7c42f3faf7e34728a89dc749d64f8
Patrick Fong <patrickf3139@gmail.com>
2021-06-16 13:53:25 -0700

    update readme

--- completions/fzf_configure_bindings.fish
+++ completions/fzf_configure_bindings.fish
@@ -1,7 +1,7 @@
 complete fzf_configure_bindings --no-files
 complete fzf_configure_bindings --long help --short h --description "Print help"
-complete fzf_configure_bindings --long directory --description "Specify a key sequence for search directory"
-complete fzf_configure_bindings --long git_log --description "Specify a key sequence for search git log"
-complete fzf_configure_bindings --long git_status --description "Specify a key sequence for search git status"
-complete fzf_configure_bindings --long history --description "Specify a key sequence for search history"
-complete fzf_configure_bindings --long variables --description "Specify a key sequence for search variables"
+complete fzf_configure_bindings --long directory --description "Change the key binding for searching directory"
+complete fzf_configure_bindings --long git_log --description "Change the key binding for searching git log"
+complete fzf_configure_bindings --long git_status --description "Change the key binding for searching git status"
+complete fzf_configure_bindings --long history --description "Change the key binding for searching history"
+complete fzf_configure_bindings --long variables --description "Change the key binding for searching variables"
