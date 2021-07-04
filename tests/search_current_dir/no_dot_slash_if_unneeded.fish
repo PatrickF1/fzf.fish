1d4bad2106e4059ff5e3bb25e8f33445a3fecd54
Patrick Fong <patrickf3139@gmail.com>
2021-07-03 23:58:46 -0700

    update tests

--- tests/search_current_dir/no_dot_slash_if_unneeded.fish
+++
@@ -1,9 +0,0 @@
-# set everything up so that a path starting with / is outputted by fzf
-mock commandline \* ""
-mock commandline "--current-token --replace --" "echo \$argv"
-mock fd \* ""
-mock fzf \* "echo /Users/patrickf"
-
-# since there is already a /, no ./ should be prepended
-set result (_fzf_search_directory)
-@test "doesn't prepend ./ if path already starts with /" "$result" = /Users/patrickf
