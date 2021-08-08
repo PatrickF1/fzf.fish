e0b0cbb649c7bfe4a7b490a4ab7285e797a513e6
Patrick Fong <patrickf3139@gmail.com>
2021-08-08 14:00:25 -0700

    Mention removing fzf_configure_bindings from config when uninstalling

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -34,8 +34,9 @@ function _fzf_uninstall --on-event fzf_uninstall
     functions --erase _fzf_uninstall _fzf_migration_message _fzf_uninstall_bindings fzf_configure_bindings
     complete --erase fzf_configure_bindings
 
-    set_color --italics cyan
-    echo "fzf.fish uninstalled"
+    set_color cyan
+    echo "fzf.fish uninstalled."
+    echo "You may need to manually remove fzf_configure_bindings from your config.fish if you were using custom key bindings."
     set_color normal
 end
 
