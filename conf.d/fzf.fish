1afae551c08932ad0e08295e68b0769c1fd3a37f
Patrick Fong <patrickf3139@gmail.com>
2021-08-14 23:03:52 -0700

    move FZF_DEFAULT_OPTS from config into wrapper

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -13,19 +13,6 @@ set --global _fzf_search_vars_command '_fzf_search_variables (set --show | psub)
 # Install the default bindings, which are mnemonic and minimally conflict with fish's preset bindings
 fzf_configure_bindings
 
-# If FZF_DEFAULT_OPTS is not set, then set some sane defaults. This also affects fzf outside of this plugin.
-# See https://github.com/junegunn/fzf#environment-variables
-if not set --query FZF_DEFAULT_OPTS
-    # cycle allows jumping between the first and last results, making scrolling faster
-    # layout=reverse lists results top to bottom, mimicking the familiar layouts of git log, history, and env
-    # border makes clear where the fzf window begins and ends
-    # height=90% leaves space to see the current command and some scrollback, maintaining context of work
-    # preview-window=wrap wraps long lines in the preview window, making reading easier
-    # marker=* makes the multi-select marker more distinguishable from the pointer (since both default to >)
-    set --global --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
-end
-
-# Doesn't erase FZF_DEFAULT_OPTS because too hard to tell if it was set by the user or by this plugin
 # Doesn't erase autoloaded _fzf_* functions because they are not easily accessible once key bindings are erased
 function _fzf_uninstall --on-event fzf_uninstall
     _fzf_uninstall_bindings
--- functions/_fzf_wrapper.fish
+++ functions/_fzf_wrapper.fish
@@ -4,5 +4,17 @@ function _fzf_wrapper --description "Prepares some environment variables before 
     # Use --local so that it doesn't clobber SHELL outside of this function.
     set --local --export SHELL (command --search fish)
 
+    # If FZF_DEFAULT_OPTS is not set, then set some sane defaults.
+    # See https://github.com/junegunn/fzf#environment-variables
+    if not set --query FZF_DEFAULT_OPTS
+        # cycle allows jumping between the first and last results, making scrolling faster
+        # layout=reverse lists results top to bottom, mimicking the familiar layouts of git log, history, and env
+        # border makes clear where the fzf window begins and ends
+        # height=90% leaves space to see the current command and some scrollback, maintaining context of work
+        # preview-window=wrap wraps long lines in the preview window, making reading easier
+        # marker=* makes the multi-select marker more distinguishable from the pointer (since both default to >)
+        set --global --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
+    end
+
     fzf $argv
 end
