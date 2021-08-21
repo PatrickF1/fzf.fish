a5f08566f801be1b6e412c75d6e66ea054952e30
Patrick Fong <patrickf3139@gmail.com>
2021-08-21 10:55:02 -0700

    readme

--- functions/_fzf_wrapper.fish
+++ functions/_fzf_wrapper.fish
@@ -1,6 +1,6 @@
-function _fzf_wrapper --description "Prepares some environment variables before invoking fzf."
-    # Make sure fzf uses fish to execute preview functions, some of which
-    # are autoloaded so don't exist in other shells.
+function _fzf_wrapper --description "Prepares some environment variables before executing fzf."
+    # Make sure fzf uses fish to execute preview commands, some of which
+    # are autoloaded fish functions so don't exist in other shells.
     # Use --local so that it doesn't clobber SHELL outside of this function.
     set --local --export SHELL (command --search fish)
 
