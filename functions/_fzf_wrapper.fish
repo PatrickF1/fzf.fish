e973f6a587e690836010284920adf60cb9590b30
Patrick Fong <patrickf3139@gmail.com>
2021-08-21 10:31:34 -0700

    simpliy sentence

--- functions/_fzf_wrapper.fish
+++ functions/_fzf_wrapper.fish
@@ -9,7 +9,7 @@ function _fzf_wrapper --description "Prepares some environment variables before 
     if not set --query FZF_DEFAULT_OPTS
         # cycle allows jumping between the first and last results, making scrolling faster
         # layout=reverse lists results top to bottom, mimicking the familiar layouts of git log, history, and env
-        # border makes clear where the fzf window begins and ends
+        # border shows where the fzf window begins and ends
         # height=90% leaves space to see the current command and some scrollback, maintaining context of work
         # preview-window=wrap wraps long lines in the preview window, making reading easier
         # marker=* makes the multi-select marker more distinguishable from the pointer (since both default to >)
