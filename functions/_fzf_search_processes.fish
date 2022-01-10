f7e70d588ead0a82fcd6f773716d36d45f46d7e8
Yonatan Striem-Amit <yonatan@cybereason.com>
2022-01-10 08:40:38 -0500

    Replace AWK with built in fish functions to reduce dependencies

--- functions/_fzf_search_processes.fish
+++ functions/_fzf_search_processes.fish
@@ -2,16 +2,16 @@ function _fzf_search_processes --description "Search pid for all running command
     set fzf_arguments --multi --ansi $fzf_processes_opts
     set token (commandline --current-token)
 
-    set pid_selected (
+    set process_selected (
         ps -A -opid,command | \
         _fzf_wrapper --query $token  \
                      --header-lines=1 \
                      --preview='ps -o pid,user,%cpu,time,rss,start,command -p {1}' \
-                     $fzf_arguments | \
-        awk '{print $1}'
+                     $fzf_arguments 
     )
 
     if test $status -eq 0
+        set pid_selected (string split -n " " $process_selected)[1]
         commandline --current-token --replace -- (string escape -- $pid_selected)
     end
 
