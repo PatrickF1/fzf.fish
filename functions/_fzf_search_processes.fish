014ba14d25c804094605ad305735a208d8befaae
Patrick Fong <patrickf3139@gmail.com>
2022-01-15 11:15:43 -0800

    fix command description and whitespace

--- functions/_fzf_search_processes.fish
+++ functions/_fzf_search_processes.fish
@@ -1,4 +1,4 @@
-function _fzf_search_processes --description "Search pid for all running commands. Replace the current token with the pid of the selected process."
+function _fzf_search_processes --description "Search all running processes. Replace the current token with the pid of the selected process."
     set fzf_arguments --multi --ansi $fzf_processes_opts
     set token (commandline --current-token)
 
@@ -7,7 +7,7 @@ function _fzf_search_processes --description "Search pid for all running command
         _fzf_wrapper --query $token  \
                      --header-lines=1 \
                      --preview='ps -o pid,user,%cpu,time,rss,start,command -p {1}' \
-                     $fzf_arguments 
+                     $fzf_arguments
     )
 
     if test $status -eq 0
