b11ba309cf39fd071bbe3dc6a9dae789abf8c9ec
Nadav Zingerman <7372858+nzig@users.noreply.github.com>
2022-01-15 17:28:21 +0200

    Allow selecting multiple git commits in log search

--- functions/_fzf_search_git_log.fish
+++ functions/_fzf_search_git_log.fish
@@ -5,18 +5,22 @@ function _fzf_search_git_log --description "Search the output of git log and pre
         # see documentation for git format placeholders at https://git-scm.com/docs/git-log#Documentation/git-log.txt-emnem
         # %h gives you the abbreviated commit hash, which is useful for saving screen space, but we will have to expand it later below
         set log_fmt_str '%C(bold blue)%h%C(reset) - %C(cyan)%ad%C(reset) %C(yellow)%d%C(reset) %C(normal)%s%C(reset)  %C(dim normal)[%an]%C(reset)'
-        set selected_log_line (
+        set selected_log_lines (
             git log --color=always --format=format:$log_fmt_str --date=short | \
             _fzf_wrapper --ansi \
+                --multi \
                 --tiebreak=index \
                 --preview='git show --color=always {1}' \
                 --query=(commandline --current-token) \
                 $fzf_git_log_opts
         )
         if test $status -eq 0
-            set abbreviated_commit_hash (string split --max 1 " " $selected_log_line)[1]
-            set commit_hash (git rev-parse $abbreviated_commit_hash)
-            commandline --current-token --replace $commit_hash
+            for line in $selected_log_lines
+                set abbreviated_commit_hash (string split --max 1 " " $line)[1]
+                set commit_hash (git rev-parse $abbreviated_commit_hash)
+                set commit_hashes $commit_hashes $commit_hash
+            end
+            commandline --current-token --replace (string join ' ' $commit_hashes)
         end
     end
 
