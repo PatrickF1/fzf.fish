92fc3308e0dff160afca6b38b5744e80d2d35d05
Patrick Fong <patrickf3139@gmail.com>
2022-01-28 22:36:00 -0800

    fix extra # in comment

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -6,7 +6,7 @@ end
 # Because of scoping rules, to capture the shell variables exactly as they are, we must read
 # them before even executing _fzf_search_variables. We use psub to store the
 # variables' info in temporary files and pass in the filenames as arguments.
-# # This variable is global so that it can be referenced by fzf_configure_bindings and in tests
+# This variable is global so that it can be referenced by fzf_configure_bindings and in tests
 set --global _fzf_search_vars_command '_fzf_search_variables (set --show | psub) (set --names | psub)'
 
 
