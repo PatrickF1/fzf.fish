5fb8a961dfe380ead7282a18412d5f9963708bac
Patrick Fong <patrickf3139@gmail.com>
2022-01-29 15:04:06 -0800

    remove --multi

--- functions/_fzf_search_processes.fish
+++ functions/_fzf_search_processes.fish
@@ -1,13 +1,13 @@
 function _fzf_search_processes --description "Search all running processes. Replace the current token with the pid of the selected process."
-    set fzf_arguments --multi --ansi $fzf_processes_opts
     set token (commandline --current-token)
 
     set process_selected (
         ps -A -opid,command | \
         _fzf_wrapper --query "$token" \
+                     --ansi \
                      --header-lines=1 \
                      --preview='ps -o pid,user,ppid,%cpu,rss,time,start,command -p {1}' \
-                     $fzf_arguments
+                     $fzf_processes_opts
     )
 
     if test $status -eq 0
