4f926c349639e74848c4ea4b2c4a2e6f0781c034
Patrick Fong <patrickf3139@gmail.com>
2022-01-29 17:45:11 -0800

    [Search processes] use clearer ps field names in preview

--- functions/_fzf_search_processes.fish
+++ functions/_fzf_search_processes.fish
@@ -1,12 +1,15 @@
 function _fzf_search_processes --description "Search all running processes. Replace the current token with the pid of the selected process."
+    # use all caps to be consistent with ps default format
+    # snake_case because ps doesn't seem to allow spaces in the field names
+    set ps_preview_fmt (string join ',' 'pid' 'ppid=PARENT_PID' 'user' '%cpu' 'rss=RSS_IN_KB' 'start=START_TIME' 'command')
     set processes_selected (
         ps -A -opid,command | \
         _fzf_wrapper --multi \
                      --query (commandline --current-token) \
                      --ansi \
-                     # first line outputted by ps is "PID COMMAND", so we need to mark that as the header
+                     # first line outputted by ps is a header, so we need to mark it as so
                      --header-lines=1 \
-                     --preview='ps -o pid,user,ppid,%cpu,rss,time,start,command -p {1}' \
+                     --preview="ps -o '$ps_preview_fmt' -p {1}" \
                      $fzf_processes_opts
     )
 
