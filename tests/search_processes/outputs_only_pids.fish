77877f4871fefb9ab43abfc25d4a18d8a27ff96e
Patrick Fong <patrickf3139@gmail.com>
2022-01-29 16:25:27 -0800

    fix test

--- tests/search_processes/outputs_only_pids.fish
+++ tests/search_processes/outputs_only_pids.fish
@@ -3,8 +3,10 @@ mock commandline \* ""
 mock commandline "--current-token --replace --" "echo \$argv"
 set --export --append FZF_DEFAULT_OPTS "--filter=''" # automatically select all input lines sent to fzf
 
-set pids (_fzf_search_processes)
+# string split output so that each pid is an array rather than a long string
+set pids (_fzf_search_processes | string split)
+
 # test that only valid numbers are outputted (i.e. nothing else polluting the output of pids)
-# this string regex will return 0 if a single non-digit character exists in the output
-string match --regex --quiet '\D' $pids
+# this string match command will return 0 if a single non-digit character exists in the output, otherwise 1
+string match --regex --quiet '\D' (string split $pids)
 @test "outputs only pids" $status -eq 1
