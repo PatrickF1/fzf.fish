d9d0dca58df3cfe3a6ad3343ad2ff0c26c74d221
Patrick Fong <patrickf3139@gmail.com>
2022-03-19 11:31:56 -0700

    also test for nested filename

--- tests/search_git_status/outputs_right_paths.fish
+++ tests/search_git_status/outputs_right_paths.fish
@@ -1,10 +1,11 @@
-set files "filename_with_*.txt" "filename with space.csv"
+set files "filename_with_*.txt" "filename with space.csv" tests/_resources/nestedfilename
 touch $files
 mock commandline \* ""
 mock commandline "--current-token --replace --" "echo \$argv"
 set --export --append FZF_DEFAULT_OPTS "--filter='filename'"
+
 set actual (_fzf_search_git_status)
-test $actual = 'filename_with_*.txt "filename with space.csv"'
+test $actual = 'filename_with_*.txt "filename with space.csv" tests/_resources/nestedfilename'
 @test "correct paths found in output" $status -eq 0
 
 rm $files
