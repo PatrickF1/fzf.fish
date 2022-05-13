702439613a0b531fa1df2ad1fb2676444cd88307
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-05-12 20:19:07 -0500

    [Search variables] Always wrap preview  (#239)
    Useful for when the variable is very long, e.g. database URI. There shouldn't be any situation where this is an inconvenience.

--- functions/_fzf_search_variables.fish
+++ functions/_fzf_search_variables.fish
@@ -23,6 +23,7 @@ function _fzf_search_variables --argument-names set_show_output set_names_output
     set variable_names_selected (
         printf '%s\n' $all_variable_names |
         _fzf_wrapper --preview "_fzf_extract_var_info {} $set_show_output" \
+            --preview-window="wrap" \
             --multi \
             --query=$cleaned_curr_token \
             $fzf_shell_vars_opts
