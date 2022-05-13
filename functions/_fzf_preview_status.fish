71554d85ea006dfab33c6e054139fc91f825bd77
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-05-14 05:46:55 +0800

    fix: regex

--- functions/_fzf_preview_status.fish
+++ functions/_fzf_preview_status.fish
@@ -1,19 +1,21 @@
 function _fzf_preview_status
-    echo $argv
     set -l path (string split ' ' $argv)[-1]
-    echo $path
     if string match -r '^\?\?' $argv >/dev/null
+        echo Untracked
+        echo
         _fzf_preview_file $path
-    else if string match -r '^A' $argv >/dev/null
+    else if string match -r '^A\s\s\S' $argv >/dev/null
+        echo -e (set_color green)New file
+        echo
         git diff --color=always -- /dev/null $path
     else
-        if string match -r '[MDTRC] \S' $argv >/dev/null
+        if string match -r '\S\s\S' $argv >/dev/null
             echo -e (set_color red)Unstaged
             echo
             git diff --color=always -- $path
             echo
         end
-        if string match -r '[AMDTRC][MDTRC ]\S' $argv >/dev/null
+        if string match -r '\S\s\s\S|\S\S\s\S' $argv >/dev/null
             echo -e (set_color green)Staged
             echo
             git diff --color=always --cached HEAD -- $path
