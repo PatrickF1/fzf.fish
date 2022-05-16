d1bc38e8af5a1ff0b1f84f0929214cb153f9a414
Next Alone <12210746+NextAlone@users.noreply.github.com>
2022-05-16 09:43:21 +0800

    fix: change helper desc

--- functions/_fzf_preview_file.fish
+++ functions/_fzf_preview_file.fish
@@ -1,4 +1,4 @@
-# helper function for _fzf_search_directory
+# helper function for _fzf_search_directory and _fzf_search_git_status
 function _fzf_preview_file --description "Print a preview for the given file based on its file type."
     # because there's no way to guarantee that _fzf_search_directory passes the path to _fzf_preview_file
     # as one argument, we collect all the arguments into one single variable and treat that as the path
