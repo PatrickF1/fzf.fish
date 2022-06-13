2c2fc22f99c0ac31f5249ac49cb6924d478b0857
Patrick Fong <patrickf3139@gmail.com>
2022-06-12 19:05:06 -0700

    update readme, make markers more readable, add comments

--- functions/_fzf_preview_changed_file.fish
+++ functions/_fzf_preview_changed_file.fish
@@ -1,20 +1,20 @@
+# helper for _fzf_search_git_status
+# input should be a line from git status --short, something like:
+# MM functions/_fzf_preview_changed_file.fish
 function _fzf_preview_changed_file
     set -l path (string split ' ' $argv)[-1]
     if string match -r '^\?\?' $argv --quiet
-        echo Untracked
-        echo
+        echo -e (set_color --underline)=== Untracked ===\n
         _fzf_preview_file $path
     else
         if string match -r '\S\s\S' $argv --quiet
-            echo -e (set_color red)Unstaged
-            echo
+            echo -e (set_color --underline red)=== Unstaged ===\n
             git diff --color=always -- $path
             echo
         end
         if string match -r '^\S' $argv --quiet
-            echo -e (set_color green)Staged
-            echo
-            git diff --color=always --cached HEAD -- $path
+            echo -e (set_color --underline green)=== Staged ===\n
+            git diff --color=always --staged -- $path
         end
     end
 end
