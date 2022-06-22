05599f6a1fab7923bd1d416958e116751dacd778
Kid <44045911+kidonng@users.noreply.github.com>
2022-06-22 10:51:03 +0800

    Add a comment

--- functions/_fzf_search_history.fish
+++ functions/_fzf_search_history.fish
@@ -1,5 +1,6 @@
 function _fzf_search_history --description "Search command history. Replace the command line with the selected command."
     # history merge incorporates history changes from other fish sessions
+    # skip merging in private mode as it clears current session history
     if test "$fish_private_mode" != 1
         builtin history merge
     end
