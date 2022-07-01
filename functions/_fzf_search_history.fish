a79dc7510d02f25360c2f30e50287abd0766a588
Patrick Fong <patrickf3139@gmail.com>
2022-07-01 11:26:18 -0700

    [Search history] correctly test if in private mode
    Fix wrong test operator and typo in variable name. Follow up to #245.

--- functions/_fzf_search_history.fish
+++ functions/_fzf_search_history.fish
@@ -1,7 +1,7 @@
 function _fzf_search_history --description "Search command history. Replace the command line with the selected command."
     # history merge incorporates history changes from other fish sessions
-    # skip merging in private mode as it clears current session history
-    if test -n "$fish_provate_mode"
+    # it errors out if called in private mode
+    if test -z "$fish_private_mode"
         builtin history merge
     end
 
