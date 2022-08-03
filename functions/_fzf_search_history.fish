ffbfaed2a092774ee5f12732929c5d372e1e1a8d
polair <michael.r.gallagher@noaa.gov>
2022-08-03 17:35:49 -0600

    requested changes

--- functions/_fzf_search_history.fish
+++ functions/_fzf_search_history.fish
@@ -5,7 +5,7 @@ function _fzf_search_history --description "Search command history. Replace the 
         builtin history merge
     end
 
-    set command_with_ts (
+    set commands_with_ts (
         # Reference https://devhints.io/strftime to understand strftime format symbols
         builtin history --null --show-time="%m-%d %H:%M:%S │ " |
         _fzf_wrapper --read0 \
@@ -20,7 +20,7 @@ function _fzf_search_history --description "Search command history. Replace the 
 
     if test $status -eq 0
         set -U commands_selected # empty list to loop over selected commands, in case multiple
-        for c in (string split \n $command_with_ts)
+        for c in $commands_with_ts
             set --append commands_selected (string split --max 1 " │ " $c)[2]
         end
         commandline --replace -- $commands_selected
