c130f691062c31dd92eaa3e52b6debf15feaf98b
polair <michael.r.gallagher@noaa.gov>
2022-08-04 00:58:19 -0600

    fix/cleanup regex and add multi

--- functions/_fzf_search_history.fish
+++ functions/_fzf_search_history.fish
@@ -9,6 +9,7 @@ function _fzf_search_history --description "Search command history. Replace the 
         # Reference https://devhints.io/strftime to understand strftime format symbols
         builtin history --null --show-time="%m-%d %H:%M:%S │ " |
         _fzf_wrapper --read0 \
+            --multi \
             --tiebreak=index \
             --query=(commandline) \
             # preview current command using fish_ident in a window at the bottom 3 lines tall
@@ -21,11 +22,7 @@ function _fzf_search_history --description "Search command history. Replace the 
     if test $status -eq 0
         set commands_selected # empty list to loop over selected commands, in case multiple
         for c in (string split \n $commands_with_ts)
-            if string match -r --quiet '(\d\d-\d\d \d\d:\d\d:\d\d)' $c
-                set --append commands_selected (string split --max 1 " │ " $c)[2]
-            else
-                set --append commands_selected $c
-            end
+            set --append commands_selected (string replace --regex '^(\d\d-\d\d \d\d:\d\d:\d\d\s│\s)' '' $c)
         end
         commandline --replace -- $commands_selected
     end
