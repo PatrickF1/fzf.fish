efa72c71a0e57aa84594f665d7bc6d41b99ab731
Patrick Fong <patrickf3139@gmail.com>
2022-09-24 23:42:18 -0700

    Fix preview for changed paths containing spaces

---
+++ tests/preview_changed_file/path_with_spaces.fish
@@ -0,0 +1,9 @@
+set path_with_space "tests/_resources/multi word dir/file 1.txt"
+set expected_diff a-very-unique-line
+echo $expected_diff >>$path_with_space
+set output (_fzf_preview_changed_file " M \"$path_with_space\"")
+
+string match --entire --quiet $expected_diff $output
+@test "git diff successfully invoked on path with space" $status -eq 0
+
+git restore $path_with_space
