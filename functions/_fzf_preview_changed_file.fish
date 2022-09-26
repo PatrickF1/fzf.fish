7f7052126a6c627222f3dba2fbe7e187e0a4dddd
Patrick Fong <patrickf3139@gmail.com>
2022-09-25 21:57:24 -0700

    support merge state

--- functions/_fzf_preview_changed_file.fish
+++ functions/_fzf_preview_changed_file.fish
@@ -12,14 +12,16 @@ function _fzf_preview_changed_file --argument-names path_status --description "S
     # https://git-scm.com/docs/git-status/2.35.0#_output
     set -l index_status (string sub --length 1 $path_status)
     set -l working_tree_status (string sub --start 2 --length 1 $path_status)
+    # no-prefix because the file is always being compared to itself so is unecessary
+    set diff_opts --color=always --no-prefix
 
     if test $index_status = '?'
         _fzf_report_diff_type Untracked
         _fzf_preview_file $path
+    else if contains {$index_status}$working_tree_status AU UD UA DU UU AA
+        _fzf_report_diff_type Unmerged
+        git diff $diff_opts -- $path
     else
-        # no-prefix because the file is always being compared to itself so is unecessary
-        set diff_opts --color=always --no-prefix
-
         if test $index_status != ' '
             _fzf_report_diff_type Staged
             git diff --staged $diff_opts -- $path
