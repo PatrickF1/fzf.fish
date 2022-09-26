1a0bf6c66ce37bfb0b4b0d96d14e958445c21448
Patrick <patrickf3139@gmail.com>
2022-09-25 23:02:14 -0700

    [Search git status] support previewing merge conflicts (#262)
    Report files containing merge conflicts as "Unmerged" when previewing to distinguish them from normal changes.
    Also, update some comments, tests, and docs related to Search git status for clarity.

--- functions/_fzf_preview_changed_file.fish
+++ functions/_fzf_preview_changed_file.fish
@@ -3,23 +3,27 @@
 # MM functions/_fzf_preview_changed_file.fish
 #  D README.md
 # R  LICENSE.md -> LICENSE
-function _fzf_preview_changed_file --description "Show the untracked, staged, and/or unstaged changes in the given file."
+function _fzf_preview_changed_file --argument-names path_status --description "Show the git diff of the given file."
     # remove quotes because they'll be interpreted literally by git diff
-    # don't requote when referencing $path because fish does not perform word splitting
+    # no need to requote when referencing $path because fish does not perform word splitting
     # https://fishshell.com/docs/current/fish_for_bash_users.html
-    set -l path (string unescape (string sub --start 4 $argv))
+    set -l path (string unescape (string sub --start 4 $path_status))
     # first letter of short format shows index, second letter shows working tree
     # https://git-scm.com/docs/git-status/2.35.0#_output
-    set -l index_status (string sub --length 1 $argv)
-    set -l working_tree_status (string sub --start 2 --length 1 $argv)
+    set -l index_status (string sub --length 1 $path_status)
+    set -l working_tree_status (string sub --start 2 --length 1 $path_status)
+    # no-prefix because the file is always being compared to itself so is unecessary
+    set diff_opts --color=always --no-prefix
 
     if test $index_status = '?'
         _fzf_report_diff_type Untracked
         _fzf_preview_file $path
+    else if contains {$index_status}$working_tree_status UD DU UU AA
+        # inferred from # https://stackoverflow.com/questions/22792906/how-do-i-produce-every-possible-git-status
+        # the above 4 statuses are the only possible ones for a merge conflict
+        _fzf_report_diff_type Unmerged
+        git diff $diff_opts -- $path
     else
-        # no-prefix because the file is always being compared to itself so is unecessary
-        set diff_opts --color=always --no-prefix
-
         if test $index_status != ' '
             _fzf_report_diff_type Staged
             git diff --staged $diff_opts -- $path
--- tests/preview_changed_file/deleted_in_working.fish
+++ tests/preview_changed_file/deleted_in_working.fish
@@ -1,7 +1,5 @@
 mock git diff "echo \$argv"
-set file dir/file.txt
+set output (_fzf_preview_changed_file " D out.log")
 
-set output (_fzf_preview_changed_file " D $file")
-
-contains -- "| Unstaged |" $output && not contains "| Staged |" $output
+contains "| Unstaged |" $output && not contains "| Staged |" $output
 @test "only shows unstaged changes if file was only deleted in working tree" $status -eq 0
