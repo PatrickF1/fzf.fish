a7e8613247d51c8bdf6fab5201f46095622c9d55
Patrick Fong <patrickf3139@gmail.com>
2022-09-25 22:57:37 -0700

    contains doesn't need --

--- tests/preview_changed_file/deleted_in_working.fish
+++ tests/preview_changed_file/deleted_in_working.fish
@@ -1,5 +1,5 @@
 mock git diff "echo \$argv"
 set output (_fzf_preview_changed_file " D out.log")
 
-contains -- "| Unstaged |" $output && not contains -- "| Staged |" $output
+contains "| Unstaged |" $output && not contains "| Staged |" $output
 @test "only shows unstaged changes if file was only deleted in working tree" $status -eq 0
--- tests/preview_changed_file/modified_in_both.fish
+++ tests/preview_changed_file/modified_in_both.fish
@@ -1,5 +1,5 @@
 mock git diff "echo \$argv"
 set output (_fzf_preview_changed_file "MM dir/file.txt")
 
-contains -- "| Unstaged |" $output && contains -- "| Staged |" $output
+contains "| Unstaged |" $output && contains "| Staged |" $output
 @test "shows staged and unstaged changes if the file is modified in both places" $status -eq 0
