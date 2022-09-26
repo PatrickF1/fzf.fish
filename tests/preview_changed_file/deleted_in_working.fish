1020c5375a9dda408fa5a91f28797af0465ccb79
Patrick Fong <patrickf3139@gmail.com>
2022-09-25 22:34:01 -0700

    remove unnecessary var in tests

--- tests/preview_changed_file/deleted_in_working.fish
+++ tests/preview_changed_file/deleted_in_working.fish
@@ -1,7 +1,5 @@
 mock git diff "echo \$argv"
-set file dir/file.txt
-
-set output (_fzf_preview_changed_file " D $file")
+set output (_fzf_preview_changed_file " D out.log")
 
 contains -- "| Unstaged |" $output && not contains "| Staged |" $output
 @test "only shows unstaged changes if file was only deleted in working tree" $status -eq 0
--- tests/preview_changed_file/modified_in_both.fish
+++ tests/preview_changed_file/modified_in_both.fish
@@ -1,6 +1,5 @@
 mock git diff "echo \$argv"
-set file dir/file.txt
-set output (_fzf_preview_changed_file "MM $file")
+set output (_fzf_preview_changed_file "MM dir/file.txt")
 
 contains -- "| Unstaged |" $output && contains "| Staged |" $output
 @test "shows staged and unstaged changes if the file is modified in both places" $status -eq 0
