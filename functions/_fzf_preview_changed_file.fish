81fae01093c5346db848074bba78be11bde8756f
Patrick Fong <patrickf3139@gmail.com>
2022-09-25 21:37:27 -0700

    give argv a name

--- functions/_fzf_preview_changed_file.fish
+++ functions/_fzf_preview_changed_file.fish
@@ -3,15 +3,15 @@
 # MM functions/_fzf_preview_changed_file.fish
 #  D README.md
 # R  LICENSE.md -> LICENSE
-function _fzf_preview_changed_file --description "Show the untracked, staged, and/or unstaged changes in the given file."
+function _fzf_preview_changed_file --argument-names path_status --description "Show the untracked, staged, and/or unstaged changes in the given file."
     # remove quotes because they'll be interpreted literally by git diff
     # don't requote when referencing $path because fish does not perform word splitting
     # https://fishshell.com/docs/current/fish_for_bash_users.html
-    set -l path (string unescape (string sub --start 4 $argv))
+    set -l path (string unescape (string sub --start 4 $path_status))
     # first letter of short format shows index, second letter shows working tree
     # https://git-scm.com/docs/git-status/2.35.0#_output
-    set -l index_status (string sub --length 1 $argv)
-    set -l working_tree_status (string sub --start 2 --length 1 $argv)
+    set -l index_status (string sub --length 1 $path_status)
+    set -l working_tree_status (string sub --start 2 --length 1 $path_status)
 
     if test $index_status = '?'
         _fzf_report_diff_type Untracked
