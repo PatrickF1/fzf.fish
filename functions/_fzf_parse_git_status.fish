5d6762fa02d9783b18f54b5c056ae1088d25dd1e
Patrick Fong <patrickf3139@gmail.com>
2022-10-08 21:46:44 -0700

    partial regex solution

--- functions/_fzf_parse_git_status.fish
+++ functions/_fzf_parse_git_status.fish
@@ -1,2 +1,9 @@
-function _fzf_parse_git_status --argument-names path_status --description "Parse the status info and path(s) from a line of git status output."
+function _fzf_parse_git_status_paths --argument-names path_status --description "Parse the path from a line of git status output. If the path was renamed, then return both the original and new path."
+    string match --regex --groups-only '(?<index_status>[ MADRU])(?<working_tree_status>[ MADRU])' (string sub --length 2 $path_status)
+    if test $status -neq 0
+        false
+    end
+
+    set raw_paths (string split -- ' -> ')
+    string match --regex --groups-only '((?<original_path>.*) -> )?(?<path>.*)$'
 end
