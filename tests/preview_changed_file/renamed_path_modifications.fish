PatrickF1/fzf.fish

commit 38896f2f84de00d950331dab11f95bd4265b7b12

[Search git status] Fix previewing paths with spaces
Previously, paths containing spaces broke the preview. Turns out the
qutoes added by git status for paths containing spaces were being passed
literally to git diff as part of the path and thus git diff could not
find the file.

functions/_fzf_preview_changed_file.fish
@@ -4,7 +4,10 @@
 #  D README.md
 # R  LICENSE.md -> LICENSE
 function _fzf_preview_changed_file --description "Show the untracked, staged, and/or unstaged changes in the given file."
-    set -l path (string split ' ' $argv)[-1]
+    # remove quotes because they'll be interpreted literally by git diff
+    # don't requote when referencing $path because fish does not perform word splitting
+    # https://fishshell.com/docs/current/fish_for_bash_users.html
+    set -l path (string unescape (string sub --start 4 $argv))
     # first letter of short format shows index, second letter shows working tree
     # https://git-scm.com/docs/git-status/2.35.0#_output
     set -l index_status (string sub --length 1 $argv)

tests/preview_changed_file/path_with_spaces.fish
@@ -1,0 +1,9 @@
+set path_with_space "tests/_resources/multi word dir/file 1.txt"
+set expected_diff a-very-unique-line
+echo $expected_diff >>$path_with_space
+set output (_fzf_preview_changed_file " M \"$path_with_space\"")
+
+string match --entire --quiet $expected_diff $output
+@test "git diff successfully invoked on path with space" $status -eq 0
+
+git restore $path_with_space

commit 3a430c0c124d8c5b074ac1b0839b367a5ce71d1e

[Search git status] Preview only changes since file renamed (#263)
Previously, the git diff preview for a renamed path shows the entire file as being added. A more useful diff is to view only the modifications made to the file content. To accomplish this, we need a special condition for renames to diff the current (post-rename) path with the original path.

In addition, this PR fixes git status previews for renamed paths, which was accidentally broken in 38896f2.

functions/_fzf_preview_changed_file.fish
@@ -2,7 +2,7 @@
 # arg should be a line from git status --short, e.g.
 # MM functions/_fzf_preview_changed_file.fish
 #  D README.md
-# R  LICENSE.md -> LICENSE
+# R  LICENSE -> "New License"
 function _fzf_preview_changed_file --argument-names path_status --description "Show the git diff of the given file."
     # remove quotes because they'll be interpreted literally by git diff
     # no need to requote when referencing $path because fish does not perform word splitting
@@ -12,8 +12,8 @@ function _fzf_preview_changed_file --argument-names path_status --description "S
     # https://git-scm.com/docs/git-status/2.35.0#_short_format
     set -l index_status (string sub --length 1 $path_status)
     set -l working_tree_status (string sub --start 2 --length 1 $path_status)
-    # no-prefix because the file is always being compared to itself so is unecessary
-    set diff_opts --color=always --no-prefix
+
+    set diff_opts --color=always
 
     if test $index_status = '?'
         _fzf_report_diff_type Untracked
@@ -27,7 +27,18 @@ function _fzf_preview_changed_file --argument-names path_status --description "S
     else
         if test $index_status != ' '
             _fzf_report_diff_type Staged
-            git diff --staged $diff_opts -- $path
+
+            # renames are only detected in the index, never working tree, so only need to test for it here
+            # https://stackoverflow.com/questions/73954214
+            if test $index_status = R
+                # diff the post-rename path with the original path, otherwise the diff will show the entire file as being added
+                set orig_and_new_path (string split --max 1 -- ' -> ' $path)
+                git diff --staged $diff_opts -- $orig_and_new_path[1] $orig_and_new_path[2]
+                # path currently has the form of "original -> current", so we need to correct it before it's used below
+                set path $orig_and_new_path[2]
+            else
+                git diff --staged $diff_opts -- $path
+            end
         end
 
         if test $working_tree_status != ' '

tests/preview_changed_file/modified_path_with_spaces.fish
@@ -4,6 +4,6 @@ echo $expected_diff >>$path_with_space
 set output (_fzf_preview_changed_file " M \"$path_with_space\"")
 
 string match --entire --quiet $expected_diff $output
-@test "git diff successfully invoked on path with space" $status -eq 0
+@test "successfully previews modified path with spaces" $status -eq 0
 
 git restore $path_with_space

tests/preview_changed_file/renamed_in_index.fish
@@ -1,15 +1,0 @@
-set --export diff_working_called 0
-set --export diff_staged_called 0
-function git
-    if test $argv[1] != diff
-        echo "$argv[1] is unsupported" >&2
-        exit 1
-    else if contains -- --staged $argv
-        set diff_staged_called 1
-    else
-        set diff_working_called 1
-    end
-end
-
-_fzf_preview_changed_file "R  file1 -> file2" >/dev/null
-@test "only calls git diff staged for renamed files" $diff_working_called -eq 0 -a $diff_staged_called -eq 1

tests/preview_changed_file/renamed_path_modifications.fish
@@ -1,0 +1,17 @@
+set orig_path "tests/_resources/alphabet 26 lines.txt"
+set renamed_path "tests/_resources/alphabet lines.txt"
+set added_line a-very-unique-line
+git mv $orig_path $renamed_path
+echo $added_line >>$renamed_path
+git add $renamed_path
+
+set output (_fzf_preview_changed_file "R  \"$orig_path\" -> \"$renamed_path\"")
+
+# test that the added line shows up in the diff but not the entire file
+# "aaaaaaaaaa" is the first line in the file
+string match --entire --quiet $added_line $output &&
+    not string match --entire aaaaaaaaaa $output
+@test "shows only the modifications made to renamed file" $status -eq 0
+
+git mv $renamed_path $orig_path
+git restore --staged --worktree $orig_path