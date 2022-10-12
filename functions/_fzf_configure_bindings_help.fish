47bd143e4e877421959dbcacbe26c2d86afd9e57
Patrick Fong <patrickf3139@gmail.com>
2022-10-12 15:56:40 -0700

    clearer language

--- functions/_fzf_configure_bindings_help.fish
+++ functions/_fzf_configure_bindings_help.fish
@@ -5,7 +5,7 @@ USAGE:
 
 DESCRIPTION
     By default, fzf_configure_bindings installs mnemonic key bindings for fzf.fish's features. Each
-    feature's binding can be customized through a corresponding namesake option:
+    feature's binding can be configured through a corresponding namesake option:
         FEATURE            |  MNEMONIC KEY SEQUENCE        |  CORRESPONDING OPTION
         Search directory   |  Ctrl+Alt+F (F for file)      |  --directory
         Search git log     |  Ctrl+Alt+L (L for log)       |  --git_log
@@ -13,17 +13,18 @@ DESCRIPTION
         Search history     |  Ctrl+R     (R for reverse)   |  --history
         Search variables   |  Ctrl+V     (V for variable)  |  --variables
         Search processes   |  Ctrl+Alt+P (P for process)   |  --processes
-    An option with a key sequence value overrides the binding for its feature, while an option
-    without a value disables the binding. A feature that is not customized retains its default
-    menomonic binding specified above. Key bindings are installed for default and insert modes.
+    Override a feature's binding by setting its corresponding option to the desired key sequence.
+    Disable a feature's binding by setting its corresponding option to an empty value. A feature
+    that is not configured retains its default menomonic binding specified above.
+    fzf_configure_bindings will erase all bindings it previously installed before installing
+    new ones so is safe to execute repeatedly in the same fish session. It installs bindings for
+    both default and insert modes.
 
     In terms of validation, fzf_configure_bindings fails if passed unknown options. Furthermore, it
     expects an equals sign between an option's name and value. However, it does not validate key
     sequences. Rather, consider using fish_key_reader to manually validate them.
 
-    In terms of experimentation, fzf_configure_bindings erases any bindings it previously installed
-    before installing new ones so it can be repeatedly executed in the same fish session without
-    problem. Once the desired fzf_configure_bindings command has been found, add it to config.fish
+    Once the desired fzf_configure_bindings command has been found, add it to config.fish
     in order to persist the bindings.
 
     The -h and --help options print this help message.
