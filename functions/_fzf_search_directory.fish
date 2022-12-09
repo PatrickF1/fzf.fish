e5155f699c8c00b95a918f7f32696fb609afd24a
Saúl Nogueras <susensio@nuc>
2022-12-09 09:56:46 +0100

    Look up fd binary

--- functions/_fzf_search_directory.fish
+++ functions/_fzf_search_directory.fish
@@ -1,4 +1,7 @@
 function _fzf_search_directory --description "Search the current directory. Replace the current token with the selected file paths."
+    # On Ubuntu and other Debian-based Linux distributions, fd binary is called fdfind.
+    set fd_cmd (command -v fd || command -v fdfind)
+
     # --string-cwd-prefix prevents fd >= 8.3.0 from prepending ./ to relative paths
     set fd_opts --color=always --strip-cwd-prefix $fzf_fd_opts
 
@@ -16,10 +19,10 @@ function _fzf_search_directory --description "Search the current directory. Repl
         set --append fd_opts --base-directory=$unescaped_exp_token
         # use the directory name as fzf's prompt to indicate the search is limited to that directory
         set --prepend fzf_arguments --prompt="Search Directory $unescaped_exp_token> " --preview="_fzf_preview_file $expanded_token{}"
-        set file_paths_selected $unescaped_exp_token(fd $fd_opts 2>/dev/null | _fzf_wrapper $fzf_arguments)
+        set file_paths_selected $unescaped_exp_token($fd_cmd $fd_opts 2>/dev/null | _fzf_wrapper $fzf_arguments)
     else
         set --prepend fzf_arguments --prompt="Search Directory> " --query="$unescaped_exp_token" --preview='_fzf_preview_file {}'
-        set file_paths_selected (fd $fd_opts 2>/dev/null | _fzf_wrapper $fzf_arguments)
+        set file_paths_selected ($fd_cmd $fd_opts 2>/dev/null | _fzf_wrapper $fzf_arguments)
     end
 
 
@@ -33,7 +36,7 @@ function _fzf_search_directory --description "Search the current directory. Repl
         if test (count $file_paths_selected) = 1
             set commandline_tokens (commandline --tokenize)
             if test "$commandline_tokens" = "$token" -a -d "$file_paths_selected" \
-                    -a (fd --version | string replace --regex --all '[^\d]' '') -lt 840
+                    -a ($fd_cmd --version | string replace --regex --all '[^\d]' '') -lt 840
                 set file_paths_selected $file_paths_selected/
             end
         end
