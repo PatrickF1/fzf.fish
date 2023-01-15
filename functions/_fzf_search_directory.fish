6f50cc1ad29d9560553b72ad0ded5b6d5d298565
Saúl Nogueras <susensio@gmail.com>
2023-01-15 09:20:20 +0100

    [Search Directory] Use fd or fdfind unaliased (#282)
    A cursory search through GitHub code for "alias fd fdfind" reveals hundreds of pieces of code that aliases fdfind to fd instead of using symlinks (in spite of fd's instructions to symlink instead). And that's just the code that's public! The problem with this is that fish functions buffer their output, so when fd is aliased, Search Directory does not pop open fzf until fd is done outputting files. If Search Directory is triggered in $HOME or some other large directory, then Search Directory appears to hang for several seconds.
    
    We fix this by using the fd binary directly. Since presumably many of fzf.fish users on certain Linux distros have it installed as fdfind, we look for either binaries and use it. Resolves #281.

--- functions/_fzf_search_directory.fish
+++ functions/_fzf_search_directory.fish
@@ -1,5 +1,8 @@
 function _fzf_search_directory --description "Search the current directory. Replace the current token with the selected file paths."
-    set fd_opts --color=always $fzf_fd_opts
+    # Directly use fd binary to avoid output buffering delay caused by a fd alias, if any.
+    # Debian-based distros install fd as fdfind so also check for that. Fall back to "fd" for a clear error message.
+    set fd_cmd (command -v fd || command -v fdfind || echo "fd")
+    set --append fd_cmd --color=always $fzf_fd_opts
 
     # $fzf_dir_opts is the deprecated version of $fzf_directory_opts
     set fzf_arguments --multi --ansi $fzf_dir_opts $fzf_directory_opts
@@ -12,13 +15,13 @@ function _fzf_search_directory --description "Search the current directory. Repl
     # If the current token is a directory and has a trailing slash,
     # then use it as fd's base directory.
     if string match --quiet -- "*/" $unescaped_exp_token && test -d "$unescaped_exp_token"
-        set --append fd_opts --base-directory=$unescaped_exp_token
+        set --append fd_cmd --base-directory=$unescaped_exp_token
         # use the directory name as fzf's prompt to indicate the search is limited to that directory
         set --prepend fzf_arguments --prompt="Search Directory $unescaped_exp_token> " --preview="_fzf_preview_file $expanded_token{}"
-        set file_paths_selected $unescaped_exp_token(fd $fd_opts 2>/dev/null | _fzf_wrapper $fzf_arguments)
+        set file_paths_selected $unescaped_exp_token($fd_cmd 2>/dev/null | _fzf_wrapper $fzf_arguments)
     else
         set --prepend fzf_arguments --prompt="Search Directory> " --query="$unescaped_exp_token" --preview='_fzf_preview_file {}'
-        set file_paths_selected (fd $fd_opts 2>/dev/null | _fzf_wrapper $fzf_arguments)
+        set file_paths_selected ($fd_cmd 2>/dev/null | _fzf_wrapper $fzf_arguments)
     end
 
 
