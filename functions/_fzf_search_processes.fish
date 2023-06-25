a63bdee294a6f266481c45f92f6179007165f152
tamirzb <tamir.z3@gmail.com>
2023-06-26 01:40:44 +0800

    [Search processes] Prefer ps binary over ps alias
    Same as done for fd in directory search

--- functions/_fzf_search_processes.fish
+++ functions/_fzf_search_processes.fish
@@ -1,9 +1,11 @@
 function _fzf_search_processes --description "Search all running processes. Replace the current token with the pid of the selected process."
+    # Prefer ps binary over ps alias
+    set -f ps_cmd (command -v ps || echo "ps")
     # use all caps to be consistent with ps default format
     # snake_case because ps doesn't seem to allow spaces in the field names
     set -f ps_preview_fmt (string join ',' 'pid' 'ppid=PARENT' 'user' '%cpu' 'rss=RSS_IN_KB' 'start=START_TIME' 'command')
     set -f processes_selected (
-        ps -A -opid,command | \
+        $ps_cmd -A -opid,command | \
         _fzf_wrapper --multi \
                     --prompt="Search Processes> " \
                     --query (commandline --current-token) \
@@ -11,7 +13,7 @@ function _fzf_search_processes --description "Search all running processes. Repl
                     # first line outputted by ps is a header, so we need to mark it as so
                     --header-lines=1 \
                     # ps uses exit code 1 if the process was not found, in which case show an message explaining so
-                    --preview="ps -o '$ps_preview_fmt' -p {1} || echo 'Cannot preview {1} because it exited.'" \
+                    --preview="$ps_cmd -o '$ps_preview_fmt' -p {1} || echo 'Cannot preview {1} because it exited.'" \
                     --preview-window="bottom:4:wrap" \
                     $fzf_processes_opts
     )
